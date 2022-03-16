6df9a6e3a0fad4dd76979ad00a707f2cf94e504d
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-03-16 16:39:23 +0100

    removed files unusefull

--- azure-devops/app-projects/locals.tf
+++
@@ -1,8 +0,0 @@
-# locals {
-#   prefix                   = "pagopa"
-#   key_vault_name           = "io-p-kv-azuredevops"
-#   key_vault_resource_group = "io-p-rg-operations"
-#   key_vault_subscription   = "PROD-IO"
-#   #tfsec:ignore:GEN002
-#   tlscert_renew_token = "v1"
-# }
