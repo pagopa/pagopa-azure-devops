96a81b51d0ad4d147ac1004a30b77ec8bfde1e3c
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-03-22 17:14:46 +0100

    better variables names for kv azdo

--- azure-devops/iac-projects/00_secrets_prod.tf
+++ azure-devops/iac-projects/00_secrets_prod.tf
@@ -6,7 +6,7 @@ module "secrets" {
   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
 
   resource_group = local.prod_key_vault_resource_group
-  key_vault_name = local.prod_key_vault_name
+  key_vault_name = local.prod_key_vault_azdo_name
 
   secrets = [
     "DANGER-GITHUB-API-TOKEN",
--- azure-devops/iac-projects/99_variables.tf
+++ azure-devops/iac-projects/99_variables.tf
@@ -28,9 +28,9 @@ locals {
   azure_devops_org = "pagopaspa"
 
   # 🔐 KV
-  dev_key_vault_name  = "${local.prefix}-d-azdo-weu-kv"
-  uat_key_vault_name  = "${local.prefix}-u-azdo-weu-kv"
-  prod_key_vault_name = "${local.prefix}-p-azdo-weu-kv"
+  dev_key_vault_azdo_name  = "${local.prefix}-d-azdo-weu-kv"
+  uat_key_vault_azdo_name  = "${local.prefix}-u-azdo-weu-kv"
+  prod_key_vault_azdo_name = "${local.prefix}-p-azdo-weu-kv"
 
   dev_key_vault_resource_group  = "${local.prefix}-d-sec-rg"
   uat_key_vault_resource_group  = "${local.prefix}-u-sec-rg"
