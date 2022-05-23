251b677c364dea83c8805af1c4c13e1b0ded2ab9
Simone Infante <simone.infante@pagopa.it>
2022-05-23 22:36:01 +0200

    feat: add service connection for aks

---
+++ azure-devops/app-projects/03_service_connections_aks.tf
@@ -0,0 +1,38 @@
+resource "azuredevops_serviceendpoint_kubernetes" "aks-dev" {
+  depends_on            = [azuredevops_project.project]
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "${var.prefix}-aks-dev"
+  apiserver_url         = module.secrets.values["aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.secrets.values["aks-azure-devops-sa-token-dev"].value
+    ca_cert = module.secrets.values["aks-azure-devops-sa-cacrt-dev"].value
+  }
+}
+
+resource "azuredevops_serviceendpoint_kubernetes" "aks-uat" {
+  depends_on            = [azuredevops_project.project]
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "${var.prefix}-aks-uat"
+  apiserver_url         = module.secrets.values["aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.secrets.values["aks-azure-devops-sa-token-uat"].value
+    ca_cert = module.secrets.values["aks-azure-devops-sa-cacrt-uat"].value
+  }
+}
+
+resource "azuredevops_serviceendpoint_kubernetes" "aks-prod" {
+  depends_on            = [azuredevops_project.project]
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "${var.prefix}-aks-prod"
+  apiserver_url         = module.secrets.values["aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.secrets.values["aks-azure-devops-sa-token-prod"].value
+    ca_cert = module.secrets.values["aks-azure-devops-sa-cacrt-prod"].value
+  }
+}
