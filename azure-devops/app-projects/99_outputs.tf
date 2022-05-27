7fc50aa67ad941d83673ea9ec25e48cb5a35e0d6
Simone Infante <simone.infante@pagopa.it>
2022-05-27 14:25:38 +0200

    feat: add service connection in outputs

---
+++ azure-devops/app-projects/99_outputs.tf
@@ -0,0 +1,28 @@
+output "service_endpoint_azure_devops_acr_aks_dev_id" {
+  value = azuredevops_serviceendpoint_azurecr.acr_aks_dev.id  
+}
+
+output "service_endpoint_azure_devops_acr_aks_dev_name" {
+  value = azuredevops_serviceendpoint_azurecr.acr_aks_dev.service_endpoint_name  
+}
+
+output "service_endpoint_azure_devops_github_rw_name" {
+  value = azuredevops_serviceendpoint_github.azure-devops-github-rw.service_endpoint_name  
+}
+
+output "service_endpoint_azure_devops_github_pr_id" {
+  value = azuredevops_serviceendpoint_github.azure-devops-github-pr.id
+}
+
+output "service_endpoint_azure_devops_github_ro_id" {
+  value = azuredevops_serviceendpoint_github.azure-devops-github-ro.id
+}
+
+output "service_endpoint_azure_devops_github_rw_id" {
+  value = azuredevops_serviceendpoint_github.azure-devops-github-rw.id
+}
+
+output "service_endpoint_azure_dev_id" {
+  value = azuredevops_serviceendpoint_azurerm.DEV-SERVICE-CONN.id
+}
+
