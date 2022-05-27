e4d46d09910a4ecb51b707e20e373e3ad3bc2567
Simone Infante <simone.infante@pagopa.it>
2022-05-27 14:27:14 +0200

    feat: add service connection for ecommere aks

--- azure-devops/ecommerce-projects/03_service_connections_aks.tf
+++ azure-devops/ecommerce-projects/03_service_connections_aks.tf
@@ -1,13 +1,13 @@
 resource "azuredevops_serviceendpoint_kubernetes" "aks_dev" {
-  depends_on            = [azuredevops_project.project]
-  project_id            = azuredevops_project.project.id
+  depends_on            = [data.azuredevops_project.project]
+  project_id            = data.azuredevops_project.project.id
   service_endpoint_name = local.srv_endpoint_name_aks_dev
-  apiserver_url         = module.ecommerce_secrets.values["pagopa-d-weu-dev-aks-apiserver-url"].value
+  apiserver_url         = module.ecommerce_dev_secrets.values["pagopa-d-weu-dev-aks-apiserver-url"].value
   authorization_type    = "ServiceAccount"
   service_account {
     # base64 values
-    token   = module.ecommerce_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-token"].value
-    ca_cert = module.ecommerce_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-cacrt"].value
+    token   = module.ecommerce_dev_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-token"].value
+    ca_cert = module.ecommerce_dev_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-cacrt"].value
   }
 }
 
