2ebb12ec42ed8f3a8a3c28d1c616369df0cdb6e8
Simone Infante <simone.infante@pagopa.it>
2022-05-27 14:26:09 +0200

    fix: read kv given a env

--- azure-devops/ecommerce-projects/00_secrets_ecommerce.tf
+++ azure-devops/ecommerce-projects/00_secrets_ecommerce.tf
@@ -2,11 +2,16 @@
 # PROD ECOMMERCE KEYVAULT
 #
 
-module "ecommerce_secrets" {
+module "ecommerce_dev_secrets" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
 
-  resource_group = local.prod_ecommerce_key_vault_resource_group
-  key_vault_name = local.prod_ecommerce_key_vault_name
+  resource_group = local.dev_ecommerce_key_vault_resource_group
+  key_vault_name = local.dev_ecommerce_key_vault_name
 
   secrets = [
     "pagopa-d-weu-dev-aks-azure-devops-sa-token",
@@ -14,3 +19,29 @@ module "ecommerce_secrets" {
     "pagopa-d-weu-dev-aks-apiserver-url"
   ]
 }
+
+# module "ecommerce_uat_secrets" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+
+#   resource_group = local.uat_ecommerce_key_vault_resource_group
+#   key_vault_name = local.uat_ecommerce_key_vault_name
+
+#   secrets = [
+#     "pagopa-u-weu-dev-aks-azure-devops-sa-token",
+#     "pagopa-u-weu-dev-aks-azure-devops-sa-cacrt",
+#     "pagopa-u-weu-dev-aks-apiserver-url"
+#   ]
+# }
+
+# module "ecommerce_prod_secrets" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+
+#   resource_group = local.prod_ecommerce_key_vault_resource_group
+#   key_vault_name = local.prod_ecommerce_key_vault_name
+
+#   secrets = [
+#     "pagopa-p-weu-dev-aks-azure-devops-sa-token",
+#     "pagopa-p-weu-dev-aks-azure-devops-sa-cacrt",
+#     "pagopa-p-weu-dev-aks-apiserver-url"
+#   ]
+# }
