fd7bf4eb034a274beaa3d69ff3ee9ed14bddd25b
Simone infante <52280205+infantesimone@users.noreply.github.com>
2022-07-07 18:41:06 +0200

    feat: New folder with ecommerce pipeline (#16)
    * feat: new secret for aks
    
    * feat: service connection for acr aks
    
    * feat: add service connection for aks
    
    * feat: add deploy pipeline for transactions-service
    
    * feat: update variable and readme
    
    * fix: typo
    
    * feat: add missing secret
    
    * fix: type in resource name
    
    * fix: aks srv endpoint name
    
    * feat: add ecommerce kv
    
    * feat: refactorgin in oredr to have a separate project for ecommerce
    
    * fix: terraform state for ecommerce
    
    * feat: add app terraform state refs for ecommerce
    
    * feat: delete old resources related to ecommerce
    
    * feat: add service connection in outputs
    
    * fix: read kv given a env
    
    * fix: remove ecommerce project  to use pagopa
    
    * feat: add service connection for ecommere aks
    
    * fix: use pagopa project instead ecommerce
    
    * fox: ecommerce service connections
    
    * feat: add deploy pipelin for sessions-service
    
    * feat: add payment instruments pipelines
    
    * fix: rename pipeline definition
    
    * feat: add pipeline for tls weudev.ecommerce
    
    * feat: update module version  ecommerce pipelines

---
+++ azure-devops/ecommerce-projects/06_pagopa-ecommerce-payments-instruments-service.tf
@@ -0,0 +1,126 @@
+variable "pagopa-ecommerce-payment-instruments-service" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "pagopa-ecommerce-payment-instruments-service"
+      branch_name     = "refs/heads/main"
+      pipelines_path  = ".devops"
+      yml_prefix_name = null
+    }
+    pipeline = {
+      enable_code_review = true
+      enable_deploy      = true
+      sonarcloud = {
+        # TODO azure devops terraform provider does not support SonarCloud service endpoint
+        service_connection = "SONARCLOUD-SERVICE-CONN"
+        org                = "pagopa"
+        project_key        = "pagopa_pagopa-ecommerce-payment-instruments-service"
+        project_name       = "pagopa-ecommerce-payment-instruments-service"
+      }
+    }
+  }
+}
+
+locals {
+  # global vars
+  pagopa-ecommerce-payment-instruments-service-variables = {
+    cache_version_id = "v1"
+    default_branch   = var.pagopa-ecommerce-payment-instruments-service.repository.branch_name
+  }
+  # global secrets
+  pagopa-ecommerce-payment-instruments-service-variables_secret = {
+
+  }
+  # code_review vars
+  pagopa-ecommerce-payment-instruments-service-variables_code_review = {
+    danger_github_api_token = "skip"
+    sonarcloud_service_conn = var.pagopa-ecommerce-payment-instruments-service.pipeline.sonarcloud.service_connection
+    sonarcloud_org          = var.pagopa-ecommerce-payment-instruments-service.pipeline.sonarcloud.org
+    sonarcloud_project_key  = var.pagopa-ecommerce-payment-instruments-service.pipeline.sonarcloud.project_key
+    sonarcloud_project_name = var.pagopa-ecommerce-payment-instruments-service.pipeline.sonarcloud.project_name
+  }
+  # code_review secrets
+  pagopa-ecommerce-payment-instruments-service-variables_secret_code_review = {
+
+  }
+  # deploy vars
+  pagopa-ecommerce-payment-instruments-service-variables_deploy = {
+    github_connection = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_name
+    tenant_id         = module.secrets.values["TENANTID"].value
+
+    # acr section
+    dev_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
+    k8s_image_repository_name           = replace(var.pagopa-ecommerce-payment-instruments-service.repository.name, "-", "")
+    dev_container_registry_name         = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_name
+
+    # uat_container_registry  = azuredevops_serviceendpoint_azurecr.acr_aks_uat.service_endpoint_name
+    # prod_container_registry = azuredevops_serviceendpoint_azurecr.acr_aks_prod.service_endpoint_name
+
+    # aks section
+    dev_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_dev.id
+
+    dev_container_namespace = "pagopapcommonacr.azurecr.io"
+    # uat_container_namespace  = "pagopapcommonacr.azurecr.io"
+    # prod_container_namespace = "pagopapcommonacr.azurecr.io"
+
+  }
+  # deploy secrets
+  pagopa-ecommerce-payment-instruments-service-variables_secret_deploy = {
+
+  }
+}
+
+module "pagopa-ecommerce-payment-instruments-service_code_review" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.2.0"
+  count  = var.pagopa-ecommerce-payment-instruments-service.pipeline.enable_code_review == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.pagopa-ecommerce-payment-instruments-service.repository
+  github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_pr_id
+  path                         = "${local.domain}\\pagopa-ecommerce-payment-instruments-service"
+
+  variables = merge(
+    local.pagopa-ecommerce-payment-instruments-service-variables,
+    local.pagopa-ecommerce-payment-instruments-service-variables_code_review,
+  )
+
+  variables_secret = merge(
+    local.pagopa-ecommerce-payment-instruments-service-variables_secret,
+    local.pagopa-ecommerce-payment-instruments-service-variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_ro_id,
+    local.azuredevops_serviceendpoint_sonarcloud_id
+  ]
+}
+
+module "pagopa-ecommerce-payment-instruments-service_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.2.0"
+  count  = var.pagopa-ecommerce-payment-instruments-service.pipeline.enable_deploy == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.pagopa-ecommerce-payment-instruments-service.repository
+  github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_id
+  path                         = "${local.domain}\\pagopa-ecommerce-payment-instruments-service"
+
+  variables = merge(
+    local.pagopa-ecommerce-payment-instruments-service-variables,
+    local.pagopa-ecommerce-payment-instruments-service-variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.pagopa-ecommerce-payment-instruments-service-variables_secret,
+    local.pagopa-ecommerce-payment-instruments-service-variables_secret_deploy,
+  )
+
+  service_connection_ids_authorization = [
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_ro_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id,
+    # azuredevops_serviceendpoint_azurecr.acr_aks_uat.id,
+    # azuredevops_serviceendpoint_azurecr.acr_aks_prod.id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_dev_id,
+    # azuredevops_serviceendpoint_azurerm.UAT-SERVICE-CONN.id,
+    # azuredevops_serviceendpoint_azurerm.PROD-SERVICE-CONN.id,
+  ]
+}
