419f95ff91314c509e87f1bed1ad15c1a8bdc734
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-07-12 18:08:28 +0200

    add tls cert pipelines for gps and add github mail user for deploy CD

--- azure-devops/app-projects/03_service_connections_tls_certificate.tf
+++ azure-devops/app-projects/03_service_connections_tls_certificate.tf
@@ -155,3 +155,44 @@ resource "azurerm_key_vault_access_policy" "DEV-ECOMMERCE-TLS-CERT-SERVICE-CONN_
 
   certificate_permissions = ["Get", "Import"]
 }
+
+
+#
+# ⛩ Service connection 2 🔐 KV-GPS@DEV 🟢
+#
+#tfsec:ignore:GEN003
+module "DEV-GPS-TLS-CERT-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v2.0.5"
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  renew_token       = local.tlscert_renew_token
+  name              = "${local.prefix}-gps-d-tls-cert-kv-policy"
+  tenant_id         = module.secrets.values["TENANTID"].value
+  subscription_id   = module.secrets.values["DEV-SUBSCRIPTION-ID"].value
+  subscription_name = var.dev_subscription_name
+
+  credential_subcription              = var.dev_subscription_name
+  credential_key_vault_name           = local.dev_gps_key_vault_name
+  credential_key_vault_resource_group = local.dev_gps_key_vault_resource_group
+}
+
+data "azurerm_key_vault" "kv_gps_dev" {
+  provider            = azurerm.dev
+  name                = local.dev_gps_key_vault_name
+  resource_group_name = local.dev_gps_key_vault_resource_group
+}
+
+resource "azurerm_key_vault_access_policy" "DEV-GPS-TLS-CERT-SERVICE-CONN_kv_dev" {
+  provider = azurerm.dev
+
+  key_vault_id = data.azurerm_key_vault.kv_gps_dev.id
+  tenant_id    = module.secrets.values["TENANTID"].value
+  object_id    = module.DEV-GPS-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
