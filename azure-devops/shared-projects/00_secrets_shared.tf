94c2b1ea18ff8e4d699d80197a26b49721dccbbe
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-07-15 11:01:02 +0200

    feat: add shared project configuration

---
+++ azure-devops/shared-projects/00_secrets_shared.tf
@@ -0,0 +1,21 @@
+#
+# PROD GPS KEYVAULT
+#
+
+module "shared_dev_secrets" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+
+  resource_group = local.dev_shared_key_vault_resource_group
+  key_vault_name = local.dev_shared_key_vault_name
+
+  secrets = [
+    "pagopa-d-weu-dev-aks-azure-devops-sa-token",
+    "pagopa-d-weu-dev-aks-azure-devops-sa-cacrt",
+    "pagopa-d-weu-dev-aks-apiserver-url"
+  ]
+}
