2ddfe59a05232fab8951960e5cfb0d68f594941f
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-07-29 13:00:50 +0200

    minor fix + add pipeline TLS cert for weuuat.internal.gps

--- azure-devops/gps-projects/03_service_connections_aks.tf
+++ azure-devops/gps-projects/03_service_connections_aks.tf
@@ -12,15 +12,15 @@ resource "azuredevops_serviceendpoint_kubernetes" "aks_dev" {
 }
 
 resource "azuredevops_serviceendpoint_kubernetes" "aks_uat" {
-  depends_on            = [azuredevops_project.project]
-  project_id            = azuredevops_project.project.id
+  depends_on            = [data.azuredevops_project.project]
+  project_id            = data.azuredevops_project.project.id
   service_endpoint_name = local.srv_endpoint_name_aks_uat
-  apiserver_url         = module.secrets.values["pagopa-u-weu-uat-aks-apiserver-url"].value
+  apiserver_url         = module.gps_uat_secrets.values["pagopa-u-weu-uat-aks-apiserver-url"].value
   authorization_type    = "ServiceAccount"
   service_account {
     # base64 values
-    token   = module.secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-token"].value
-    ca_cert = module.secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-cacrt"].value
+    token   = module.gps_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-token"].value
+    ca_cert = module.gps_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-cacrt"].value
   }
 }
 
