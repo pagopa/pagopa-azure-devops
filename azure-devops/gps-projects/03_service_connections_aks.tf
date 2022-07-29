afdc7191c0e96241a76bbd70d5b3119744ebfea9
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-07-29 12:40:50 +0200

    add UAT env to GDP devops pipelines

--- azure-devops/gps-projects/03_service_connections_aks.tf
+++ azure-devops/gps-projects/03_service_connections_aks.tf
@@ -11,18 +11,18 @@ resource "azuredevops_serviceendpoint_kubernetes" "aks_dev" {
   }
 }
 
-# resource "azuredevops_serviceendpoint_kubernetes" "aks_uat" {
-#   depends_on            = [azuredevops_project.project]
-#   project_id            = azuredevops_project.project.id
-#   service_endpoint_name = local.srv_endpoint_name_aks_uat
-#   apiserver_url         = module.secrets.values["aks-apiserver-url"].value
-#   authorization_type    = "ServiceAccount"
-#   service_account {
-#     # base64 values
-#     token   = module.secrets.values["aks-azure-devops-sa-token-uat"].value
-#     ca_cert = module.secrets.values["aks-azure-devops-sa-cacrt-uat"].value
-#   }
-# }
+resource "azuredevops_serviceendpoint_kubernetes" "aks_uat" {
+  depends_on            = [azuredevops_project.project]
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = local.srv_endpoint_name_aks_uat
+  apiserver_url         = module.secrets.values["pagopa-u-weu-uat-aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-token"].value
+    ca_cert = module.secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-cacrt"].value
+  }
+}
 
 # resource "azuredevops_serviceendpoint_kubernetes" "aks_prod" {
 #   depends_on            = [azuredevops_project.project]
