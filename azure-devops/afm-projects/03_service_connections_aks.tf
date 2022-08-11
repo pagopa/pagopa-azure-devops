9548e0e2a3d4b574b7581a1e08b198e4111f50aa
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-08-11 10:12:32 +0200

    wip

--- azure-devops/afm-projects/03_service_connections_aks.tf
+++ azure-devops/afm-projects/03_service_connections_aks.tf
@@ -11,28 +11,28 @@ resource "azuredevops_serviceendpoint_kubernetes" "aks_dev" {
   }
 }
 
-# resource "azuredevops_serviceendpoint_kubernetes" "aks_uat" {
-#   depends_on            = [azuredevops_project.project]
-#   project_id            = azuredevops_project.project.id
-#   service_endpoint_name = local.srv_endpoint_name_aks_uat
-#   apiserver_url         = module.secrets.values["aks-apiserver-url"].value
-#   authorization_type    = "ServiceAccount"
-#   service_account {
-#     # base64 values
-#     token   = module.secrets.values["aks-azure-devops-sa-token-uat"].value
-#     ca_cert = module.secrets.values["aks-azure-devops-sa-cacrt-uat"].value
-#   }
-# }
+resource "azuredevops_serviceendpoint_kubernetes" "aks_uat" {
+  depends_on            = [data.azuredevops_project.project]
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = local.srv_endpoint_name_aks_uat
+  apiserver_url         = module.afm_uat_secrets.values["pagopa-u-weu-uat-aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.afm_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-token"].value
+    ca_cert = module.afm_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-cacrt"].value
+  }
+}
 
-# resource "azuredevops_serviceendpoint_kubernetes" "aks_prod" {
-#   depends_on            = [azuredevops_project.project]
-#   project_id            = azuredevops_project.project.id
-#   service_endpoint_name = local.srv_endpoint_name_aks_prod
-#   apiserver_url         = module.secrets.values["aks-apiserver-url"].value
-#   authorization_type    = "ServiceAccount"
-#   service_account {
-#     # base64 values
-#     token   = module.secrets.values["aks-azure-devops-sa-token-prod"].value
-#     ca_cert = module.secrets.values["aks-azure-devops-sa-cacrt-prod"].value
-#   }
-# }
+resource "azuredevops_serviceendpoint_kubernetes" "aks_prod" {
+  depends_on            = [data.azuredevops_project.project]
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = local.srv_endpoint_name_aks_prod
+  apiserver_url         = module.afm_prod_secrets.values["pagopa-p-weu-prod-aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.secrets.values["pagopa-p-weu-prod-aks-azure-devops-sa-token"].value
+    ca_cert = module.secrets.values["pagopa-p-weu-prod-aks-azure-devops-sa-cacrt"].value
+  }
+}
