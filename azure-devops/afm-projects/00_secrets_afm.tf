723c2113d17b4cb7676df10b69944a2695cb8bb0
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-08-11 12:28:38 +0200

    fix pipelines for DEV env

--- azure-devops/afm-projects/00_secrets_afm.tf
+++ azure-devops/afm-projects/00_secrets_afm.tf
@@ -20,36 +20,36 @@ module "afm_dev_secrets" {
   ]
 }
 
-module "afm_uat_secrets" {
-  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+# module "afm_uat_secrets" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
 
-  providers = {
-    azurerm = azurerm.uat
-  }
+#   providers = {
+#     azurerm = azurerm.uat
+#   }
 
-  resource_group = local.uat_afm_key_vault_resource_group
-  key_vault_name = local.uat_afm_key_vault_name
+#   resource_group = local.uat_afm_key_vault_resource_group
+#   key_vault_name = local.uat_afm_key_vault_name
 
-  secrets = [
-    "pagopa-u-weu-uat-aks-azure-devops-sa-token",
-    "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
-    "pagopa-u-weu-uat-aks-apiserver-url"
-  ]
-}
+#   secrets = [
+#     "pagopa-u-weu-uat-aks-azure-devops-sa-token",
+#     "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
+#     "pagopa-u-weu-uat-aks-apiserver-url"
+#   ]
+# }
 
-module "afm_prod_secrets" {
-  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+# module "afm_prod_secrets" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
 
-  providers = {
-    azurerm = azurerm.prod
-  }
+#   providers = {
+#     azurerm = azurerm.prod
+#   }
 
-  resource_group = local.prod_afm_key_vault_resource_group
-  key_vault_name = local.prod_afm_key_vault_name
+#   resource_group = local.prod_afm_key_vault_resource_group
+#   key_vault_name = local.prod_afm_key_vault_name
 
-  secrets = [
-    "pagopa-p-weu-prod-aks-azure-devops-sa-token",
-    "pagopa-p-weu-prod-aks-azure-devops-sa-cacrt",
-    "pagopa-p-weu-prod-aks-apiserver-url"
-  ]
-}
+#   secrets = [
+#     "pagopa-p-weu-prod-aks-azure-devops-sa-token",
+#     "pagopa-p-weu-prod-aks-azure-devops-sa-cacrt",
+#     "pagopa-p-weu-prod-aks-apiserver-url"
+#   ]
+# }
--- azure-devops/afm-projects/06_pagopa-afm-marketplace-be-service.tf
+++ azure-devops/afm-projects/06_pagopa-afm-marketplace-be-service.tf
@@ -50,20 +50,20 @@ locals {
     tenant_id         = module.secrets.values["TENANTID"].value
 
     # acr section
-    image_repository_name                = replace(var.pagopa-afm-marketplace-be-service.repository.name, "-", "")
-    dev_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
-    uat_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
-    prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
+    image_repository_name               = replace(var.pagopa-afm-marketplace-be-service.repository.name, "-", "")
+    dev_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
+    # uat_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
+    # prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
 
     # aks section
-    k8s_namespace                = "afm"
-    dev_kubernetes_service_conn  = azuredevops_serviceendpoint_kubernetes.aks_dev.id
-    uat_kubernetes_service_conn  = azuredevops_serviceendpoint_kubernetes.aks_uat.id
-    prod_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_prod.id
+    k8s_namespace               = "afm"
+    dev_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_dev.id
+    # uat_kubernetes_service_conn  = azuredevops_serviceendpoint_kubernetes.aks_uat.id
+    # prod_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_prod.id
 
-    dev_container_namespace  = "pagopadcommonacr.azurecr.io"
-    uat_container_namespace  = "pagopaucommonacr.azurecr.io"
-    prod_container_namespace = "pagopapcommonacr.azurecr.io"
+    dev_container_namespace = "pagopadcommonacr.azurecr.io"
+    # uat_container_namespace  = "pagopaucommonacr.azurecr.io"
+    # prod_container_namespace = "pagopapcommonacr.azurecr.io"
 
   }
   # deploy secrets
