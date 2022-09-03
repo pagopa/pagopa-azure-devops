ba2683da31f86caf14e29265344e620e5091ed00
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-09-03 17:17:58 +0200

    fix terraform.sh init

--- azure-devops/terraform.sh
+++ azure-devops/terraform.sh
@@ -46,15 +46,15 @@ az account set -s "${subscription}"
 
 if echo "init plan apply refresh import output state taint destroy" | grep -w "$ACTION" > /dev/null; then
   if [ "$ACTION" = "init" ]; then
-    echo "[INFO] init tf"
-    terraform "$ACTION" -backend-config="${BACKEND_CONFIG_PATH}" "$other"
+    echo "[INFO] init tf on folder $(pwd)"
+    terraform "$ACTION" -backend-config="${BACKEND_CONFIG_PATH}" $other
   elif [ "$ACTION" = "output" ] || [ "$ACTION" = "state" ] || [ "$ACTION" = "taint" ]; then
     # init terraform backend
     terraform init -reconfigure -backend-config="${BACKEND_CONFIG_PATH}"
     terraform "$ACTION" "$other"
   else
     # init terraform backend
-    echo "[INFO] init tf"
+    echo "[INFO] init tf on folder $(pwd)"
     terraform init -reconfigure -backend-config="${BACKEND_CONFIG_PATH}"
 
     echo "[INFO] run tf with: ${ACTION} and other: >${other}<"
