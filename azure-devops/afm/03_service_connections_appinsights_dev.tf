fafd0a111bd99b33760210b2c75fa9842e6c1af5
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-09-06 18:10:34 +0200

    upd afm calculator service CD pipelines vars

---
+++ azure-devops/afm/03_service_connections_appinsights_dev.tf
@@ -0,0 +1,35 @@
+#
+# ⛩ Service connection 2 🔐 KV@DEV 🛑
+#
+#tfsec:ignore:GEN003
+module "DEV-APPINSIGHTS-SERVICE-CONN" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v2.0.5"
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  project_id = data.azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  renew_token       = local.appinsights_renew_token
+  name              = "${local.prefix}-d-${local.domain}-appinsights"
+  tenant_id         = module.secrets.values["TENANTID"].value
+  subscription_id   = module.secrets.values["DEV-SUBSCRIPTION-ID"].value
+  subscription_name = var.dev_subscription_name
+
+  credential_subcription              = var.dev_subscription_name
+  credential_key_vault_name           = local.dev_afm_key_vault_name
+  credential_key_vault_resource_group = local.dev_afm_key_vault_resource_group
+}
+
+data "azurerm_application_insights" "application_insights_dev" {
+  provider            = azurerm.dev
+  name                = local.dev_appinsights_name
+  resource_group_name = local.dev_appinsights_resource_group
+}
+
+resource "azurerm_role_assignment" "appinsights_component_contributor_dev" {
+  provider             = azurerm.dev
+  scope                = data.azurerm_application_insights.application_insights_dev.id
+  role_definition_name = "Application Insights Component Contributor"
+  principal_id         = module.DEV-APPINSIGHTS-SERVICE-CONN.service_principal_object_id
+}
