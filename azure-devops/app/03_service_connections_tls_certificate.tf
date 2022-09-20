362833927d21d898982ed0b5a22b9f4cc2b71571
Simone Infante <simone.infante@pagopa.it>
2022-09-20 23:27:05 +0200

    feat: pipilene for uat.internal.ecommerce

--- azure-devops/app/03_service_connections_tls_certificate.tf
+++ azure-devops/app/03_service_connections_tls_certificate.tf
@@ -156,6 +156,45 @@ resource "azurerm_key_vault_access_policy" "DEV-ECOMMERCE-TLS-CERT-SERVICE-CONN_
   certificate_permissions = ["Get", "Import"]
 }
 
+#
+# ⛩ Service connection 2 🔐 KV-ECOMMERCE@UAT 🟢
+#
+#tfsec:ignore:GEN003
+module "UAT-ECOMMERCE-TLS-CERT-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v2.0.5"
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  renew_token       = local.tlscert_renew_token
+  name              = "${local.prefix}-ecommerce-u-tls-cert-kv-policy"
+  tenant_id         = module.secrets.values["TENANTID"].value
+  subscription_id   = module.secrets.values["UAT-SUBSCRIPTION-ID"].value
+  subscription_name = var.uat_subscription_name
+
+  credential_subcription              = var.uat_subscription_name
+  credential_key_vault_name           = local.uat_ecommerce_key_vault_name
+  credential_key_vault_resource_group = local.uat_ecommerce_key_vault_resource_group
+}
+
+data "azurerm_key_vault" "kv_ecommerce_uat" {
+  provider            = azurerm.uat
+  name                = local.uat_ecommerce_key_vault_name
+  resource_group_name = local.uat_ecommerce_key_vault_resource_group
+}
+
+resource "azurerm_key_vault_access_policy" "UAT-ECOMMERCE-TLS-CERT-SERVICE-CONN_kv_uat" {
+  provider = azurerm.uat
+
+  key_vault_id = data.azurerm_key_vault.kv_ecommerce_uat.id
+  tenant_id    = module.secrets.values["TENANTID"].value
+  object_id    = module.UAT-ECOMMERCE-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
 
 #
 # ⛩ Service connection 2 🔐 KV-GPS@DEV 🟢
