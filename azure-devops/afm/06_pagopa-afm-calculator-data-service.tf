ba606d4e80d42727210e03cc593103fb63c8c32d
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-09-30 10:53:26 +0200

    added prod_container_registry_service_conn

--- azure-devops/afm/06_pagopa-afm-calculator-data-service.tf
+++ azure-devops/afm/06_pagopa-afm-calculator-data-service.tf
@@ -56,7 +56,7 @@ locals {
     image_repository_name               = replace(var.pagopa-afm-calculator-data-service.repository.name, "-", "")
     dev_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
     uat_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
-    # prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
+    prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
 
     # aks section
     k8s_namespace                = "afm"
