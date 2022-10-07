7083c937815a64a3cb728408c782b1083ffbafa9
Simone infante <52280205+infantesimone@users.noreply.github.com>
2022-10-07 17:06:24 +0200

    feat: Rename ecommerce methods  [CHK-662] (#74)

--- azure-devops/afm/06_pagopa-afm-calculator-data-service.tf
+++ azure-devops/afm/06_pagopa-afm-calculator-data-service.tf
@@ -53,9 +53,9 @@ locals {
     tenant_id         = module.secrets.values["TENANTID"].value
 
     # acr section
-    image_repository_name               = replace(var.pagopa-afm-calculator-data-service.repository.name, "-", "")
-    dev_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
-    uat_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
+    image_repository_name                = replace(var.pagopa-afm-calculator-data-service.repository.name, "-", "")
+    dev_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
+    uat_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
     prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
 
     # aks section
