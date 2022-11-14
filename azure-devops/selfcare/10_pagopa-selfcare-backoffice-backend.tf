8607747683f43fcf98ac823a825dc7d9a0eb66f5
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2022-11-14 22:44:19 +0100

    fix: Selfcare uat env pipelines (#94)
    * feat: Adding uat env for Selfcare-Pagopa
    
    * fix: Update UAT Selfcare

--- azure-devops/selfcare/10_pagopa-selfcare-backoffice-backend.tf
+++ azure-devops/selfcare/10_pagopa-selfcare-backoffice-backend.tf
@@ -57,7 +57,7 @@ locals {
     # acr section
     image_repository_name               = replace(var.pagopa-selfcare-backoffice-backend.repository.name, "-", "")
     dev_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
-    # uat_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
+    uat_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
     prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
 
     # custom section
@@ -77,13 +77,14 @@ locals {
     prod_subscription_id                = module.secrets.values["PROD-SUBSCRIPTION-ID"].value
 
     # aks section
-    k8s_namespace               = "selfcare"
-    dev_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_dev.id
-    # uat_kubernetes_service_conn  = azuredevops_serviceendpoint_kubernetes.aks_uat.id
+
+    k8s_namespace                = "selfcare"
+    dev_kubernetes_service_conn  = azuredevops_serviceendpoint_kubernetes.aks_dev.id
+    uat_kubernetes_service_conn  = azuredevops_serviceendpoint_kubernetes.aks_uat.id
     prod_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_prod.id
 
-    dev_container_namespace = "pagopadcommonacr.azurecr.io"
-    # uat_container_namespace  = "pagopaucommonacr.azurecr.io"
+    dev_container_namespace  = "pagopadcommonacr.azurecr.io"
+    uat_container_namespace  = "pagopaucommonacr.azurecr.io"
     prod_container_namespace = "pagopapcommonacr.azurecr.io"
 
     # apim
