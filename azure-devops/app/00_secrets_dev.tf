1aa5a4e3b306ad271e05fa44929fbdcd2b8c6b5b
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-01-12 15:25:01 +0100

    feat: PAGOPA-209 Add var to pipeline [API-CFG] (#120)
    * add var to pipeline
    
    * fix typo env
    
    Co-authored-by: Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>

--- azure-devops/app/00_secrets_dev.tf
+++ azure-devops/app/00_secrets_dev.tf
@@ -12,3 +12,18 @@ module "pagopa-node-forwarder_dev_secrets" {
     "node-forwarder-api-subscription-key",
   ]
 }
+
+module "pagopa-api-config_dev_secrets" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  resource_group = local.dev_key_vault_resource_group
+  key_vault_name = local.dev_key_vault_name
+
+  secrets = [
+    "api-config-fe-storage-account-key"
+  ]
+}
