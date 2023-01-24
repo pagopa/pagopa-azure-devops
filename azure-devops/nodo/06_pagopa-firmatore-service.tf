9f930e2a3a86e81d2131bb4d7b47e8eba7d7c7d6
Ignazio Bricco <121177316+ignaziobricco@users.noreply.github.com>
2023-01-24 15:11:52 +0100

    feat: Add pagopa-firmatore-service (#144)

---
+++ azure-devops/nodo/06_pagopa-firmatore-service.tf
@@ -0,0 +1,95 @@
+variable "pagopa-firmatore-service" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "pagopa-firmatore"
+      branch_name     = "refs/heads/develop"
+      pipelines_path  = ".devops"
+      yml_prefix_name = null
+    }
+    pipeline = {
+      enable_code_review = false
+      enable_deploy      = true
+    }
+  }
+}
+
+locals {
+  # global vars
+  pagopa-firmatore-service-variables = {
+    cache_version_id = "v1"
+    default_branch   = var.pagopa-firmatore-service.repository.branch_name
+  }
+  # global secrets
+  pagopa-firmatore-service-variables_secret = {
+
+  }
+
+  # deploy vars
+  pagopa-firmatore-service-variables_deploy = {
+    git_mail          = module.secrets.values["azure-devops-github-EMAIL"].value
+    git_username      = module.secrets.values["azure-devops-github-USERNAME"].value
+    github_connection = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_name
+    tenant_id         = module.secrets.values["TENANTID"].value
+
+    # acr section
+    image_repository_name                     = replace(var.pagopa-firmatore-service.repository.name, "-", "")
+    container-registry-service-connection-dev = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
+    repository                                = replace(var.pagopa-firmatore-service.repository.name, "-", "")
+
+    dev_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
+    uat_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
+    # prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
+
+    # aks section
+    k8s_namespace               = "nodo"
+    dev_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_dev.id
+    uat_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_uat.id
+    # prod_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_prod.id
+
+    dev_container_namespace = "pagopadcommonacr.azurecr.io"
+    uat_container_namespace = "pagopaucommonacr.azurecr.io"
+    # prod_container_namespace = "pagopapcommonacr.azurecr.io"
+
+    # nodo4 variables of cd pipeline
+    kv-service-connection-dev         = "DEV-PAGOPA-SERVICE-CONN"
+    az-kv-name-dev                    = local.dev_nodo_key_vault_name # kv name
+    kubernetes-service-connection-dev = azuredevops_serviceendpoint_kubernetes.aks_dev.id
+    deploy-pool-dev                   = "pagopa-dev-linux"
+  }
+  # deploy secrets
+  pagopa-firmatore-service-variables_secret_deploy = {
+
+  }
+}
+
+
+module "pagopa-firmatore-service_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.2.0"
+  count  = var.pagopa-firmatore-service.pipeline.enable_deploy == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.pagopa-firmatore-service.repository
+  github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_id
+  path                         = "${local.domain}\\pagopa-firmatore-service"
+
+  variables = merge(
+    local.pagopa-firmatore-service-variables,
+    local.pagopa-firmatore-service-variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.pagopa-firmatore-service-variables_secret,
+    local.pagopa-firmatore-service-variables_secret_deploy,
+  )
+
+  service_connection_ids_authorization = [
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_ro_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_dev_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_uat_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_prod_id,
+  ]
+}
