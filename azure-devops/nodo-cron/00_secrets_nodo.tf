3dff4984b3f2b1e077f4acec0c8eae1c0a7fa9ce
Ignazio Bricco <121177316+ignaziobricco@users.noreply.github.com>
2023-01-26 18:51:05 +0100

    feat: Aggiunte tf conf per UAT (#147)
    Co-authored-by: pasqualespica <36746022+pasqualespica@users.noreply.github.com>

--- azure-devops/nodo-cron/00_secrets_nodo.tf
+++ azure-devops/nodo-cron/00_secrets_nodo.tf
@@ -20,28 +20,28 @@ module "nodo_dev_secrets" {
   ]
 }
 
-# #
-# # UAT NODO KEYVAULT
-# #
-
-# module "nodo_uat_secrets" {
-#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
-
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
-
-#   resource_group = local.uat_nodo_key_vault_resource_group
-#   key_vault_name = local.uat_nodo_key_vault_name
-
-#   secrets = [
-#     "pagopa-u-weu-uat-aks-azure-devops-sa-token",
-#     "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
-#     "pagopa-u-weu-uat-aks-apiserver-url"
-#   ]
-# }
-
 #
+# UAT NODO KEYVAULT
+#
+
+module "nodo_uat_secrets" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  resource_group = local.uat_nodo_key_vault_resource_group
+  key_vault_name = local.uat_nodo_key_vault_name
+
+  secrets = [
+    "pagopa-u-weu-uat-aks-azure-devops-sa-token",
+    "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
+    "pagopa-u-weu-uat-aks-apiserver-url"
+  ]
+}
+
+
 ##
 ## PROD NODO KEYVAULT
 ##
