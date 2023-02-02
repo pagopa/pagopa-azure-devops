2a40a006015e75914362aea3a362fc3307f7d7c4
Pietro Tota <pietro.tota@pagopa.it>
2023-02-02 13:31:42 +0100

    fix: pre-commit

--- azure-devops/iac/04_iac_nodo.tf
+++ azure-devops/iac/04_iac_nodo.tf
@@ -52,7 +52,7 @@ locals {
     tf_prod_aks_azure_devops_sa_cacrt = module.nodo_prod_secrets.values["pagopa-p-weu-prod-aks-azure-devops-sa-cacrt"].value,
     tf_prod_aks_azure_devops_sa_token = base64decode(module.nodo_prod_secrets.values["pagopa-p-weu-prod-aks-azure-devops-sa-token"].value),
     tf_aks_prod_name                  = var.aks_prod_platform_name
-    tf_prod_azure_service_connection = azuredevops_serviceendpoint_azurerm.PROD-SERVICE-CONN.service_endpoint_name
+    tf_prod_azure_service_connection  = azuredevops_serviceendpoint_azurerm.PROD-SERVICE-CONN.service_endpoint_name
   }
   # global secrets
   nodo_iac_variables_secret = {}
