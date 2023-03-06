9be85c1c64d36f3856679cc0f63081a3ca494db8
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-03-06 18:07:25 +0100

    commented uat/prod PAGOPA-601

--- azure-devops/apiconfig/00_secrets_apiconfig.tf
+++ azure-devops/apiconfig/00_secrets_apiconfig.tf
@@ -24,41 +24,41 @@ module "nodo_dev_secrets" {
 # UAT NODO KEYVAULT
 #
 
-module "nodo_uat_secrets" {
-  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
-
-  providers = {
-    azurerm = azurerm.uat
-  }
-
-  resource_group = local.uat_apiconfig_key_vault_resource_group
-  key_vault_name = local.uat_apiconfig_key_vault_name
-
-  secrets = [
-    "pagopa-u-weu-uat-aks-azure-devops-sa-token",
-    "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
-    "pagopa-u-weu-uat-aks-apiserver-url"
-  ]
-}
+#module "nodo_uat_secrets" {
+#  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+#
+#  providers = {
+#    azurerm = azurerm.uat
+#  }
+#
+#  resource_group = local.uat_apiconfig_key_vault_resource_group
+#  key_vault_name = local.uat_apiconfig_key_vault_name
+#
+#  secrets = [
+#    "pagopa-u-weu-uat-aks-azure-devops-sa-token",
+#    "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
+#    "pagopa-u-weu-uat-aks-apiserver-url"
+#  ]
+#}
 
 
 #
 # PROD NODO KEYVAULT
 #
 
-module "nodo_prod_secrets" {
-  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
-
-  providers = {
-    azurerm = azurerm.prod
-  }
-
-  resource_group = local.prod_apiconfig_key_vault_resource_group
-  key_vault_name = local.prod_apiconfig_key_vault_name
-
-  secrets = [
-    "pagopa-p-weu-prod-aks-azure-devops-sa-token",
-    "pagopa-p-weu-prod-aks-azure-devops-sa-cacrt",
-    "pagopa-p-weu-prod-aks-apiserver-url"
-  ]
-}
+#module "nodo_prod_secrets" {
+#  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+#
+#  providers = {
+#    azurerm = azurerm.prod
+#  }
+#
+#  resource_group = local.prod_apiconfig_key_vault_resource_group
+#  key_vault_name = local.prod_apiconfig_key_vault_name
+#
+#  secrets = [
+#    "pagopa-p-weu-prod-aks-azure-devops-sa-token",
+#    "pagopa-p-weu-prod-aks-azure-devops-sa-cacrt",
+#    "pagopa-p-weu-prod-aks-apiserver-url"
+#  ]
+#}
