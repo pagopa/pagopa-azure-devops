ca4e86fadc39824fa5f60b08315a82f151f933c3
Simone Infante <simone.infante@pagopa.it>
2023-03-31 17:43:34 +0200

    feat: delete old fn ecommerce and renaming ed

--- azure-devops/ecommerce/06_pagopa-ecommerce-event-dispatcher-service.tf
+++ azure-devops/ecommerce/06_pagopa-ecommerce-event-dispatcher-service.tf
@@ -1,8 +1,8 @@
-variable "pagopa-ecommerce-scheduler-service" {
+variable "pagopa-ecommerce-event-dispatcher-service" {
   default = {
     repository = {
       organization    = "pagopa"
-      name            = "pagopa-ecommerce-scheduler-service"
+      name            = "pagopa-ecommerce-event-dispatcher-service"
       branch_name     = "refs/heads/main"
       pipelines_path  = ".devops"
       yml_prefix_name = null
@@ -14,8 +14,8 @@ variable "pagopa-ecommerce-scheduler-service" {
         # TODO azure devops terraform provider does not support SonarCloud service endpoint
         service_connection = "SONARCLOUD-SERVICE-CONN"
         org                = "pagopa"
-        project_key        = "pagopa_pagopa-ecommerce-scheduler-service"
-        project_name       = "pagopa-ecommerce-scheduler-service"
+        project_key        = "pagopa_pagopa-ecommerce-event-dispatcher-service"
+        project_name       = "pagopa-ecommerce-event-dispatcher-service"
       }
     }
   }
@@ -23,32 +23,32 @@ variable "pagopa-ecommerce-scheduler-service" {
 
 locals {
   # global vars
-  pagopa-ecommerce-scheduler-service-variables = {
+  pagopa-ecommerce-event-dispatcher-service-variables = {
     cache_version_id = "v1"
-    default_branch   = var.pagopa-ecommerce-scheduler-service.repository.branch_name
+    default_branch   = var.pagopa-ecommerce-event-dispatcher-service.repository.branch_name
   }
   # global secrets
-  pagopa-ecommerce-scheduler-service-variables_secret = {
+  pagopa-ecommerce-event-dispatcher-service-variables_secret = {
 
   }
   # code_review vars
-  pagopa-ecommerce-scheduler-service-variables_code_review = {
+  pagopa-ecommerce-event-dispatcher-service-variables_code_review = {
     danger_github_api_token = "skip"
-    sonarcloud_service_conn = var.pagopa-ecommerce-scheduler-service.pipeline.sonarcloud.service_connection
-    sonarcloud_org          = var.pagopa-ecommerce-scheduler-service.pipeline.sonarcloud.org
-    sonarcloud_project_key  = var.pagopa-ecommerce-scheduler-service.pipeline.sonarcloud.project_key
-    sonarcloud_project_name = var.pagopa-ecommerce-scheduler-service.pipeline.sonarcloud.project_name
+    sonarcloud_service_conn = var.pagopa-ecommerce-event-dispatcher-service.pipeline.sonarcloud.service_connection
+    sonarcloud_org          = var.pagopa-ecommerce-event-dispatcher-service.pipeline.sonarcloud.org
+    sonarcloud_project_key  = var.pagopa-ecommerce-event-dispatcher-service.pipeline.sonarcloud.project_key
+    sonarcloud_project_name = var.pagopa-ecommerce-event-dispatcher-service.pipeline.sonarcloud.project_name
   }
   # code_review secrets
-  pagopa-ecommerce-scheduler-service-variables_secret_code_review = {
+  pagopa-ecommerce-event-dispatcher-service-variables_secret_code_review = {
 
   }
   # deploy vars
-  pagopa-ecommerce-scheduler-service-variables_deploy = {
+  pagopa-ecommerce-event-dispatcher-service-variables_deploy = {
     github_connection = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_name
 
     # acr section
-    k8s_image_repository_name           = replace(var.pagopa-ecommerce-scheduler-service.repository.name, "-", "")
+    k8s_image_repository_name           = replace(var.pagopa-ecommerce-event-dispatcher-service.repository.name, "-", "")
     dev_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
     dev_container_registry_name         = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_name
     uat_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
@@ -64,30 +64,30 @@ locals {
 
   }
   # deploy secrets
-  pagopa-ecommerce-scheduler-service-variables_secret_deploy = {
+  pagopa-ecommerce-event-dispatcher-service-variables_secret_deploy = {
     git_mail     = module.secrets.values["azure-devops-github-EMAIL"].value
     git_username = module.secrets.values["azure-devops-github-USERNAME"].value
     tenant_id    = module.secrets.values["TENANTID"].value
   }
 }
 
-module "pagopa-ecommerce-scheduler-service_code_review" {
+module "pagopa-ecommerce-event-dispatcher-service_code_review" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.4.0"
-  count  = var.pagopa-ecommerce-scheduler-service.pipeline.enable_code_review == true ? 1 : 0
+  count  = var.pagopa-ecommerce-event-dispatcher-service.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
-  repository                   = var.pagopa-ecommerce-scheduler-service.repository
+  repository                   = var.pagopa-ecommerce-event-dispatcher-service.repository
   github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_pr_id
-  path                         = "${local.domain}\\pagopa-ecommerce-scheduler-service"
+  path                         = "${local.domain}\\pagopa-ecommerce-event-dispatcher-service"
 
   variables = merge(
-    local.pagopa-ecommerce-scheduler-service-variables,
-    local.pagopa-ecommerce-scheduler-service-variables_code_review,
+    local.pagopa-ecommerce-event-dispatcher-service-variables,
+    local.pagopa-ecommerce-event-dispatcher-service-variables_code_review,
   )
 
   variables_secret = merge(
-    local.pagopa-ecommerce-scheduler-service-variables_secret,
-    local.pagopa-ecommerce-scheduler-service-variables_secret_code_review,
+    local.pagopa-ecommerce-event-dispatcher-service-variables_secret,
+    local.pagopa-ecommerce-event-dispatcher-service-variables_secret_code_review,
   )
 
   service_connection_ids_authorization = [
@@ -96,23 +96,23 @@ module "pagopa-ecommerce-scheduler-service_code_review" {
   ]
 }
 
-module "pagopa-ecommerce-scheduler-service_deploy" {
+module "pagopa-ecommerce-event-dispatcher-service_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.4.0"
-  count  = var.pagopa-ecommerce-scheduler-service.pipeline.enable_deploy == true ? 1 : 0
+  count  = var.pagopa-ecommerce-event-dispatcher-service.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
-  repository                   = var.pagopa-ecommerce-scheduler-service.repository
+  repository                   = var.pagopa-ecommerce-event-dispatcher-service.repository
   github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_id
-  path                         = "${local.domain}\\pagopa-ecommerce-scheduler-service"
+  path                         = "${local.domain}\\pagopa-ecommerce-event-dispatcher-service"
 
   variables = merge(
-    local.pagopa-ecommerce-scheduler-service-variables,
-    local.pagopa-ecommerce-scheduler-service-variables_deploy,
+    local.pagopa-ecommerce-event-dispatcher-service-variables,
+    local.pagopa-ecommerce-event-dispatcher-service-variables_deploy,
   )
 
   variables_secret = merge(
-    local.pagopa-ecommerce-scheduler-service-variables_secret,
-    local.pagopa-ecommerce-scheduler-service-variables_secret_deploy,
+    local.pagopa-ecommerce-event-dispatcher-service-variables_secret,
+    local.pagopa-ecommerce-event-dispatcher-service-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
