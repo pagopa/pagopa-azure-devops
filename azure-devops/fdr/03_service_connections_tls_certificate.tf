5f095233223645cbde37a4152f0f2a998e5b3af3
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2023-04-17 14:26:42 +0200

    create pipe for weuuat.fdr.internal.uat.platform.pagopa.it

--- azure-devops/fdr/03_service_connections_tls_certificate.tf
+++ azure-devops/fdr/03_service_connections_tls_certificate.tf
@@ -45,52 +45,52 @@ module "letsencrypt_dev" {
   subscription_name = var.dev_subscription_name
 }
 
-# #
-# # UAT
-# #
-# module "UAT-FDR-TLS-CERT-SERVICE-CONN" {
+#
+# UAT
+#
+module "UAT-FDR-TLS-CERT-SERVICE-CONN" {
 
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
+  providers = {
+    azurerm = azurerm.uat
+  }
 
-#   depends_on = [data.azuredevops_project.project]
-#   source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v2.6.5"
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v2.6.5"
 
-#   project_id        = data.azuredevops_project.project.id
-#   name              = "${local.prefix}-u-${local.domain}-tls-cert"
-#   tenant_id         = module.secrets.values["TENANTID"].value
-#   subscription_name = var.uat_subscription_name
-#   subscription_id   = module.secrets.values["UAT-SUBSCRIPTION-ID"].value
-#   #tfsec:ignore:GEN003
-#   renew_token = local.tlscert_renew_token
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-u-${local.domain}-tls-cert"
+  tenant_id         = module.secrets.values["TENANTID"].value
+  subscription_name = var.uat_subscription_name
+  subscription_id   = module.secrets.values["UAT-SUBSCRIPTION-ID"].value
+  #tfsec:ignore:GEN003
+  renew_token = local.tlscert_renew_token
 
-#   credential_subcription              = var.uat_subscription_name
-#   credential_key_vault_name           = local.uat_fdr_key_vault_name
-#   credential_key_vault_resource_group = local.uat_fdr_key_vault_resource_group
-# }
+  credential_subcription              = var.uat_subscription_name
+  credential_key_vault_name           = local.uat_fdr_key_vault_name
+  credential_key_vault_resource_group = local.uat_fdr_key_vault_resource_group
+}
 
-# resource "azurerm_key_vault_access_policy" "UAT-FDR-TLS-CERT-SERVICE-CONN_kv_access_policy" {
-#   provider     = azurerm.uat
-#   key_vault_id = data.azurerm_key_vault.domain_kv_uat.id
-#   tenant_id    = module.secrets.values["TENANTID"].value
-#   object_id    = module.UAT-FDR-TLS-CERT-SERVICE-CONN.service_principal_object_id
+resource "azurerm_key_vault_access_policy" "UAT-FDR-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.uat
+  key_vault_id = data.azurerm_key_vault.domain_kv_uat.id
+  tenant_id    = module.secrets.values["TENANTID"].value
+  object_id    = module.UAT-FDR-TLS-CERT-SERVICE-CONN.service_principal_object_id
 
-#   certificate_permissions = ["Get", "Import"]
-# }
+  certificate_permissions = ["Get", "Import"]
+}
 
-# # create let's encrypt credential used to create SSL certificates
-# module "letsencrypt_uat" {
-#   source = "git::https://github.com/pagopa/azurerm.git//letsencrypt_credential?ref=v2.18.0"
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_uat" {
+  source = "git::https://github.com/pagopa/azurerm.git//letsencrypt_credential?ref=v2.18.0"
 
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
-#   prefix            = local.prefix
-#   env               = "u"
-#   key_vault_name    = local.uat_fdr_key_vault_name
-#   subscription_name = var.uat_subscription_name
-# }
+  providers = {
+    azurerm = azurerm.uat
+  }
+  prefix            = local.prefix
+  env               = "u"
+  key_vault_name    = local.uat_fdr_key_vault_name
+  subscription_name = var.uat_subscription_name
+}
 
 # #
 # # PROD
