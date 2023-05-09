5aaffd06c9b33717f94a458d0767cead62cd134e
Simone Infante <simone.infante@pagopa.it>
2023-05-09 18:44:07 +0200

    feat: update import script

--- azure-devops/checkout/_import_script.sh
+++ azure-devops/checkout/_import_script.sh
@@ -2,7 +2,12 @@
 
 bash terraform.sh init prod
 
-### pagopa-proxy
+### pagopa-proxy import
 
 # bash terraform.sh import checkout 'module.pagopa-proxy_code_review[0].azuredevops_build_definition.pipeline' 'ffd7c346-c610-46c5-969c-a1b518de20bb/566
 # bash terraform.sh import checkout 'module.pagopa-proxy_deploy[0].azuredevops_build_definition.pipeline' 'ffd7c346-c610-46c5-969c-a1b518de20bb/589
+
+### rm in old state
+# terraform state rm 'module.pagopa-proxy_code_review[0].azuredevops_build_definition.pipeline'
+# terraform state rm 'module.pagopa-proxy_deploy[0].azuredevops_build_definition.pipeline'
+
