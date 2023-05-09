4a85b5c52887bc56888cc6a0f3980234562c249e
Simone Infante <simone.infante@pagopa.it>
2023-05-09 19:22:47 +0200

    feat: checkout tests

--- azure-devops/checkout/_import_script.sh
+++ azure-devops/checkout/_import_script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-bash terraform.sh init prod
+bash terraform.sh init checkout
 
 ### pagopa-proxy import
 
 # bash terraform.sh import checkout 'module.pagopa-proxy_code_review[0].azuredevops_build_definition.pipeline' 'ffd7c346-c610-46c5-969c-a1b518de20bb/566
 # bash terraform.sh import checkout 'module.pagopa-proxy_deploy[0].azuredevops_build_definition.pipeline' 'ffd7c346-c610-46c5-969c-a1b518de20bb/589
+# bash terraform.sh import checkout 'module.pagopa-checkout-tests_code_review[0].azuredevops_build_definition.pipeline' 'ffd7c346-c610-46c5-969c-a1b518de20bb/564
+
 
 ### rm in old state
 # terraform state rm 'module.pagopa-proxy_code_review[0].azuredevops_build_definition.pipeline'
 # terraform state rm 'module.pagopa-proxy_deploy[0].azuredevops_build_definition.pipeline'
+# terraform state rm 'module.pagopa-checkout-tests_code_review[0].azuredevops_build_definition.pipeline'
 
