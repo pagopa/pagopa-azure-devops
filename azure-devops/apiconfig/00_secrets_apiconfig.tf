a245889a102b691b341d75c35843da0cc1e5996f
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-05-19 11:52:58 +0200

    enabled prod

--- azure-devops/apiconfig/00_secrets_apiconfig.tf
+++ azure-devops/apiconfig/00_secrets_apiconfig.tf
@@ -41,20 +41,20 @@ module "apiconfig_uat_secrets" {
   ]
 }
 
-# module "apiconfig_prod_secrets" {
-#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+ module "apiconfig_prod_secrets" {
+   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
 
-#   providers = {
-#     azurerm = azurerm.prod
-#   }
+   providers = {
+     azurerm = azurerm.prod
+   }
 
-#   resource_group = local.prod_apiconfig_key_vault_resource_group
-#   key_vault_name = local.prod_apiconfig_key_vault_name
+   resource_group = local.prod_apiconfig_key_vault_resource_group
+   key_vault_name = local.prod_apiconfig_key_vault_name
 
-#   secrets = [
-#     "pagopa-p-weu-prod-aks-azure-devops-sa-token",
-#     "pagopa-p-weu-prod-aks-azure-devops-sa-cacrt",
-#     "pagopa-p-weu-prod-aks-apiserver-url",
-#     "github-token-read-packages"
-#   ]
-# }
+   secrets = [
+     "pagopa-p-weu-prod-aks-azure-devops-sa-token",
+     "pagopa-p-weu-prod-aks-azure-devops-sa-cacrt",
+     "pagopa-p-weu-prod-aks-apiserver-url",
+     "github-token-read-packages"
+   ]
+ }
