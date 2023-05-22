bf5188a4e613e5a375c196bbd0d778a74248f79e
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2023-05-22 16:18:43 +0200

    fix

--- azure-devops/app/05_tlscert-uat-uat-checkout-pagopa-it.tf
+++ azure-devops/app/05_tlscert-uat-uat-checkout-pagopa-it.tf
@@ -50,7 +50,7 @@ module "tlscert-uat-checkout-pagopa-it-cert_az" {
   repository = var.tlscert-uat-checkout-pagopa-it.repository
   name       = "${var.tlscert-uat-checkout-pagopa-it.pipeline.dns_record_name}.${var.tlscert-uat-checkout-pagopa-it.pipeline.dns_zone_name}"
   #tfsec:ignore:GEN003
-  renew_token                  = local.tlscert_renew_token
+  renew_token                  = "v2" // local.tlscert_renew_token
   path                         = var.tlscert-uat-checkout-pagopa-it.pipeline.path
   github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-ro.id
 
