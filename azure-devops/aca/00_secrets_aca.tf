0a21aadb58c55443ec4d7a5ab674cb09afa8068b
Pietro Tota <pietro.tota@pagopa.it>
2023-06-06 09:58:54 +0200

    feat: add aca pipelines

---
+++ azure-devops/aca/00_secrets_aca.tf
@@ -0,0 +1,35 @@
+module "aca_dev_secrets" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+
+  resource_group = local.dev_aca_key_vault_resource_group
+  key_vault_name = local.dev_aca_key_vault_name
+
+  secrets = [
+    "pagopa-d-weu-dev-aks-azure-devops-sa-token",
+    "pagopa-d-weu-dev-aks-azure-devops-sa-cacrt",
+    "pagopa-d-weu-dev-aks-apiserver-url"
+  ]
+}
+
+module "aca_uat_secrets" {
+
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+
+  resource_group = local.uat_aca_key_vault_resource_group
+  key_vault_name = local.uat_aca_key_vault_name
+
+  secrets = [
+    "pagopa-u-weu-uat-aks-azure-devops-sa-token",
+    "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
+    "pagopa-u-weu-uat-aks-apiserver-url"
+  ]
+}
