a6db9016dbe3b6dd64b666639fdddec4ab3f61b8
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-06-06 17:30:28 +0200

    feat: CHK-1583 ACA pipelines (#204)
    Co-authored-by: Simone Infante <simone.infante@pagopa.it>

---
+++ azure-devops/aca/03_service_connections_aks.tf
@@ -0,0 +1,25 @@
+resource "azuredevops_serviceendpoint_kubernetes" "aks_dev" {
+  depends_on            = [data.azuredevops_project.project]
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = local.srv_endpoint_name_aks_dev
+  apiserver_url         = module.aca_dev_secrets.values["pagopa-d-weu-dev-aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.aca_dev_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-token"].value
+    ca_cert = module.aca_dev_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-cacrt"].value
+  }
+}
+
+resource "azuredevops_serviceendpoint_kubernetes" "aks_uat" {
+  depends_on            = [data.azuredevops_project.project]
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = local.srv_endpoint_name_aks_uat
+  apiserver_url         = module.aca_uat_secrets.values["pagopa-u-weu-uat-aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.aca_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-token"].value
+    ca_cert = module.aca_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-cacrt"].value
+  }
+}
