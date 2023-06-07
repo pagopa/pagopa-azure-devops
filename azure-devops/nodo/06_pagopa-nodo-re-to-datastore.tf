3e4a9a4b71c432ba9dd458c3157336521cef6af6
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-06-07 10:52:16 +0200

    NOD-241
    Riversare dati event hub RE in Cosmos

---
+++ azure-devops/nodo/06_pagopa-nodo-re-to-datastore.tf
@@ -0,0 +1,118 @@
+variable "pagopa-nodo-re-to-datastore" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "pagopa-nodo-re-to-datastore"
+      branch_name     = "refs/heads/main"
+      pipelines_path  = ".devops"
+      yml_prefix_name = null
+    }
+    pipeline = {
+      # TODO to be replaced by the configuration under commented
+      integration_test = {
+        enabled               = true
+        name                  = "gpd-reporting-services.integration-test"
+        pipeline_yml_filename = "integration-test-pipelines.yml"
+      }
+      enable_code_review = true
+      enable_deploy      = true
+      sonarcloud = {
+        # TODO azure devops terraform provider does not support SonarCloud service endpoint
+        service_connection = "SONARCLOUD-SERVICE-CONN"
+        org                = "pagopa"
+        project_key        = "pagopa_pagopa-nodo4-nodo-dei-pagamenti"
+        project_name       = "pagopa-nodo4-nodo-dei-pagamenti"
+      }
+    }
+  }
+}
+
+locals {
+  pagopa-nodo-re-to-datastore-variables = {
+    default_branch = var.pagopa-nodo-service.repository.branch_name
+  }
+  pagopa-nodo-re-to-datastore-variables_secret = {
+
+  }
+  pagopa-nodo-re-to-datastore-variables_code_review = {
+    danger_github_api_token = "skip"
+    sonarcloud_service_conn = var.pagopa-nodo-re-to-datastore.pipeline.sonarcloud.service_connection
+    sonarcloud_org          = var.pagopa-nodo-re-to-datastore.pipeline.sonarcloud.org
+    sonarcloud_project_key  = var.pagopa-nodo-re-to-datastore.pipeline.sonarcloud.project_key
+    sonarcloud_project_name = var.pagopa-nodo-re-to-datastore.pipeline.sonarcloud.project_name
+    # nodo4 variables of cd pipeline
+  }
+  pagopa-nodo-re-to-datastore-variables_secret_code_review = {
+
+  }
+
+  pagopa-nodo-re-to-datastore-variables_deploy = {
+    dev_azure_subscription  = azuredevops_serviceendpoint_azurerm.DEV-SERVICE-CONN.service_endpoint_name
+    uat_azure_subscription  = azuredevops_serviceendpoint_azurerm.UAT-SERVICE-CONN.service_endpoint_name
+    prod_azure_subscription = azuredevops_serviceendpoint_azurerm.PROD-SERVICE-CONN.service_endpoint_name
+  }
+  pagopa-nodo-re-to-datastore-variables_secret_deploy = {
+
+  }
+}
+
+module "pagopa-nodo-re-to-datastore_code_review" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=add_ci_trigger_code_review"
+  count  = var.pagopa-nodo-re-to-datastore.pipeline.enable_code_review == true ? 1 : 0
+
+  project_id = data.azuredevops_project.project.id
+  repository = var.pagopa-nodo-re-to-datastore.repository
+  # github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_pr_id
+  github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_id
+  path                         = "${local.domain}\\pagopa-nodo-re-to-datastore-service"
+
+  pull_request_trigger_use_yaml = true
+  ci_trigger_use_yaml           = true
+
+  variables = merge(
+    local.pagopa-nodo-re-to-datastore-variables,
+    local.pagopa-nodo-re-to-datastore-variables_code_review,
+  )
+
+  variables_secret = merge(
+    local.pagopa-nodo-re-to-datastore-variables_secret,
+    local.pagopa-nodo-re-to-datastore-variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_ro_id,
+    local.azuredevops_serviceendpoint_sonarcloud_id
+  ]
+}
+module "pagopa-nodo-re-to-datastore_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.2.0"
+  count  = var.pagopa-nodo-re-to-datastore.pipeline.enable_deploy == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.pagopa-nodo-re-to-datastore.repository
+  github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_id
+  path                         = "${local.domain}\\pagopa-nodo-re-to-datastore-service"
+
+  variables = merge(
+    local.pagopa-nodo-re-to-datastore-variables,
+    local.pagopa-nodo-re-to-datastore-variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.pagopa-nodo-re-to-datastore-variables_secret,
+    local.pagopa-nodo-re-to-datastore-variables_secret_deploy,
+  )
+
+  service_connection_ids_authorization = [
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_ro_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_dev_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_uat_id,
+    data.terraform_remote_state.app.outputs.service_endpoint_azure_prod_id,
+    module.DEV-APPINSIGHTS-SERVICE-CONN.service_endpoint_id,
+    module.UAT-APPINSIGHTS-SERVICE-CONN.service_endpoint_id,
+    module.PROD-APPINSIGHTS-SERVICE-CONN.service_endpoint_id
+  ]
+}
