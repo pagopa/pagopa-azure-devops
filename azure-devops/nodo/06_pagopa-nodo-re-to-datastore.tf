f714229bab20a7ce3d5b4c761fc1eae5be44c729
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-06-15 10:14:33 +0200

    fix template ci version

--- azure-devops/nodo/06_pagopa-nodo-re-to-datastore.tf
+++ azure-devops/nodo/06_pagopa-nodo-re-to-datastore.tf
@@ -50,12 +50,11 @@ locals {
 }
 
 module "pagopa-nodo-re-to-datastore_code_review" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=add_ci_trigger_code_review"
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.7.0"
   count  = var.pagopa-nodo-re-to-datastore.pipeline.enable_code_review == true ? 1 : 0
 
   project_id = data.azuredevops_project.project.id
   repository = var.pagopa-nodo-re-to-datastore.repository
-  # github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_pr_id
   github_service_connection_id = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_id
   path                         = "${local.domain}\\pagopa-nodo-re-to-datastore-service"
 
@@ -78,7 +77,7 @@ module "pagopa-nodo-re-to-datastore_code_review" {
   ]
 }
 module "pagopa-nodo-re-to-datastore_deploy" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.2.0"
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.7.0"
   count  = var.pagopa-nodo-re-to-datastore.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
