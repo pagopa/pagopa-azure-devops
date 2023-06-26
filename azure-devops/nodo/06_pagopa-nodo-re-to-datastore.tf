0a267acbe8ba1b0d3d6a12145788e055ad023538
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-06-26 12:33:43 +0200

    wip

--- azure-devops/nodo/06_pagopa-nodo-re-to-datastore.tf
+++ azure-devops/nodo/06_pagopa-nodo-re-to-datastore.tf
@@ -8,7 +8,7 @@ variable "pagopa-nodo-re-to-datastore" {
       yml_prefix_name = null
     }
     pipeline = {
-      enable_code_review = true
+      enable_code_review = false
       enable_deploy      = true
       sonarcloud = {
         service_connection = "SONARCLOUD-SERVICE-CONN"
@@ -36,14 +36,42 @@ locals {
     # nodo4 variables of cd pipeline
   }
   pagopa-nodo-re-to-datastore-variables_secret_code_review = {
-
   }
 
   pagopa-nodo-re-to-datastore-variables_deploy = {
+    git_mail          = module.secrets.values["azure-devops-github-EMAIL"].value
+    git_username      = module.secrets.values["azure-devops-github-USERNAME"].value
+    github_connection = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_github_rw_name
+    tenant_id         = module.secrets.values["TENANTID"].value
+
     dev_azure_subscription  = data.terraform_remote_state.app.outputs.service_endpoint_azure_dev_name
     uat_azure_subscription  = data.terraform_remote_state.app.outputs.service_endpoint_azure_uat_name
     prod_azure_subscription = data.terraform_remote_state.app.outputs.service_endpoint_azure_prod_name
+
+    image_repository_name                = replace(var.pagopa-nodo-re-to-datastore.repository.name, "-", "")
+    dev_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_dev_id
+    uat_container_registry_service_conn  = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_uat_id
+    prod_container_registry_service_conn = data.terraform_remote_state.app.outputs.service_endpoint_azure_devops_acr_aks_prod_id
+
+    dev_container_namespace  = "pagopadcommonacr.azurecr.io"
+    uat_container_namespace  = "pagopaucommonacr.azurecr.io"
+    prod_container_namespace = "pagopapcommonacr.azurecr.io"
+
+    dev_web_app_name  = "pagopa-d-weu"
+    uat_web_app_name  = "pagopa-u-weu"
+    prod_web_app_name = "pagopa-p-weu"
+
+    TF_APPINSIGHTS_SERVICE_CONN_DEV = module.DEV-APPINSIGHTS-SERVICE-CONN.service_endpoint_name
+    TF_APPINSIGHTS_RESOURCE_ID_DEV  = data.azurerm_application_insights.application_insights_dev.id
+
+    TF_APPINSIGHTS_SERVICE_CONN_UAT = module.UAT-APPINSIGHTS-SERVICE-CONN.service_endpoint_name
+    TF_APPINSIGHTS_RESOURCE_ID_UAT  = data.azurerm_application_insights.application_insights_uat.id
+
+    TF_APPINSIGHTS_SERVICE_CONN_PROD = module.PROD-APPINSIGHTS-SERVICE-CONN.service_endpoint_name
+    TF_APPINSIGHTS_RESOURCE_ID_PROD  = data.azurerm_application_insights.application_insights_prod.id
   }
+
+  # deploy secrets
   pagopa-nodo-re-to-datastore-variables_secret_deploy = {
 
   }
