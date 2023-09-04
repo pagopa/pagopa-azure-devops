fcd16f2121e330a8ef0bc037ce80e29bb9799296
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2023-09-04 18:20:34 +0200

    minor fix useless apicfg app folder ref

--- azure-devops/app/00_secrets_dev.tf
+++ azure-devops/app/00_secrets_dev.tf
@@ -13,20 +13,20 @@ module "pagopa-node-forwarder_dev_secrets" {
   ]
 }
 
-module "pagopa-api-config_dev_secrets" {
-  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+# module "pagopa-api-config_dev_secrets" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
 
-  providers = {
-    azurerm = azurerm.dev
-  }
+#   providers = {
+#     azurerm = azurerm.dev
+#   }
 
-  resource_group = local.dev_key_vault_resource_group
-  key_vault_name = local.dev_key_vault_name
+#   resource_group = local.dev_key_vault_resource_group
+#   key_vault_name = local.dev_key_vault_name
 
-  secrets = [
-    "api-config-fe-storage-account-key"
-  ]
-}
+#   secrets = [
+#     "api-config-fe-storage-account-key"
+#   ]
+# }
 
 module "pagopa-debt-position_dev_secrets" {
   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
