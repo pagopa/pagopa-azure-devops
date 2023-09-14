1c9ca1940d521b8ef16e79ad6c2927a2d48c6c32
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2023-09-15 00:39:07 +0200

    feat: Add tls cert qi uat (#258)

--- azure-devops/qi/00_secrets_qi.tf
+++ azure-devops/qi/00_secrets_qi.tf
@@ -16,20 +16,20 @@ module "qi_dev_secrets" {
   ]
 }
 
-# module "qi_uat_secrets" {
+module "qi_uat_secrets" {
 
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
+  providers = {
+    azurerm = azurerm.uat
+  }
 
-#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.4"
 
-#   resource_group = local.uat_qi_key_vault_resource_group
-#   key_vault_name = local.uat_qi_key_vault_name
+  resource_group = local.uat_qi_key_vault_resource_group
+  key_vault_name = local.uat_qi_key_vault_name
 
-#   secrets = [
-#     "pagopa-u-weu-uat-aks-azure-devops-sa-token",
-#     "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
-#     "pagopa-u-weu-uat-aks-apiserver-url"
-#   ]
-# }
+  secrets = [
+    "pagopa-u-weu-uat-aks-azure-devops-sa-token",
+    "pagopa-u-weu-uat-aks-azure-devops-sa-cacrt",
+    "pagopa-u-weu-uat-aks-apiserver-url"
+  ]
+}
