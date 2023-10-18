2d549e41d8dfd557a373ed4df60769587ff7cc74
Fernando-Granato <fernando.granato@pagopa.it>
2023-10-18 16:30:58 +0200

    fix: source for module pagopa-wallet-tests_code_review

--- azure-devops/wallet/06_pagopa-wallet-tests.tf
+++ azure-devops/wallet/06_pagopa-wallet-tests.tf
@@ -34,7 +34,7 @@ locals {
 }
 
 module "pagopa-wallet-tests_code_review" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.2.0"
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_generic?ref=v2.6.3"
   count  = var.pagopa-wallet-tests.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
