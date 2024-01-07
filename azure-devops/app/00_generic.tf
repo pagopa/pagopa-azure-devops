ae48a310c9d9a5355fa95cd9ae2798bd108c754c
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-08 00:46:48 +0100

    now subscription is loaded by resources

--- azure-devops/app/00_generic.tf
+++ azure-devops/app/00_generic.tf
@@ -1,13 +1,18 @@
 data "azurerm_client_config" "current" {}
 
-data "azurerm_subscriptions" "prod" {
-  display_name_prefix = local.prod_subscription_name
+data "azurerm_subscriptions" "dev" {
+  display_name_prefix = local.dev_subscription_name
+}
+
+output "ciao" {
+  value = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
 }
 
 data "azurerm_subscriptions" "uat" {
   display_name_prefix = local.uat_subscription_name
 }
-
-data "azurerm_subscriptions" "dev" {
-  display_name_prefix = local.dev_subscription_name
+data "azurerm_subscriptions" "prod" {
+  display_name_prefix = local.prod_subscription_name
 }
+
+
