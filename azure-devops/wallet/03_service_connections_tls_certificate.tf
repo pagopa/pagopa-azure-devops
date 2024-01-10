f1508ee87faa9a909c05afb9db38a9cc6eee0495
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-10 15:36:42 +0100

    migrated wallet

--- azure-devops/wallet/03_service_connections_tls_certificate.tf
+++ azure-devops/wallet/03_service_connections_tls_certificate.tf
@@ -8,19 +8,16 @@ module "DEV-WALLET-TLS-CERT-SERVICE-CONN" {
   }
 
   depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v4.1.5"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.1.5"
 
   project_id        = data.azuredevops_project.project.id
   name              = "${local.prefix}-d-${local.domain}-tls-cert"
   tenant_id         = data.azurerm_client_config.current.tenant_id
   subscription_name = var.dev_subscription_name
   subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
-  #tfsec:ignore:GEN003
-  renew_token = local.tlscert_renew_token
 
-  credential_subcription              = var.dev_subscription_name
-  credential_key_vault_name           = local.dev_wallet_key_vault_name
-  credential_key_vault_resource_group = local.dev_wallet_key_vault_resource_group
+    location            = var.location
+  resource_group_name = local.dev_identity_rg_name
 }
 
 resource "azurerm_key_vault_access_policy" "DEV-WALLET-TLS-CERT-SERVICE-CONN_kv_access_policy" {
@@ -54,19 +51,16 @@ module "UAT-WALLET-TLS-CERT-SERVICE-CONN" {
   }
 
   depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v4.1.5"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.1.5"
 
   project_id        = data.azuredevops_project.project.id
   name              = "${local.prefix}-u-${local.domain}-tls-cert"
   tenant_id         = data.azurerm_client_config.current.tenant_id
   subscription_name = var.uat_subscription_name
   subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
-  #tfsec:ignore:GEN003
-  renew_token = local.tlscert_renew_token
 
-  credential_subcription              = var.uat_subscription_name
-  credential_key_vault_name           = local.uat_wallet_key_vault_name
-  credential_key_vault_resource_group = local.uat_wallet_key_vault_resource_group
+      location            = var.location
+  resource_group_name = local.uat_identity_rg_name
 }
 
 resource "azurerm_key_vault_access_policy" "UAT-WALLET-TLS-CERT-SERVICE-CONN_kv_access_policy" {
