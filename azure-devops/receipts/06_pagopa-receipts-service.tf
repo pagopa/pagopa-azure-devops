34055c47012d18f09c3c5b2f47bbfb21268907f6
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-11 09:21:46 +0100

    receipts migrated to v3

--- azure-devops/receipts/06_pagopa-receipts-service.tf
+++ azure-devops/receipts/06_pagopa-receipts-service.tf
@@ -47,7 +47,7 @@ locals {
 
 
 module "pagopa-receipt-pdf-service_performance_test" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_generic?ref=v2.6.3"
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_generic?ref=v4.1.5"
   count  = var.pagopa-receipt-pdf-service.pipeline.performance_test.enabled == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
