fb0eeca8c91d83ac965baccdae36ba29b9d4f65d
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-15 12:33:06 +0100

    minor fix

--- azure-devops/afm/99_variables.tf
+++ azure-devops/afm/99_variables.tf
@@ -1,37 +1,3 @@
-variable "dev_subscription_name" {
-  type        = string
-  description = "DEV Subscription name"
-}
-
-variable "uat_subscription_name" {
-  type        = string
-  description = "UAT Subscription name"
-}
-
-variable "prod_subscription_name" {
-  type        = string
-  description = "PROD Subscription name"
-}
-
-variable "project_name" {
-  type        = string
-  description = "Project name (e.g. pagoPA platform)"
-}
-
-variable "pipeline_environments" {
-  type        = list(any)
-  description = "List of environments pipeline to create"
-}
-
-variable "terraform_remote_state_app" {
-  type = object({
-    resource_group_name  = string,
-    storage_account_name = string,
-    container_name       = string,
-    key                  = string
-  })
-}
-
 locals {
   prefix           = "pagopa"
   azure_devops_org = "pagopaspa"
@@ -94,4 +60,39 @@ locals {
   dev_appinsights_resource_group  = "${local.prefix}-d-monitor-rg"
   uat_appinsights_resource_group  = "${local.prefix}-u-monitor-rg"
   prod_appinsights_resource_group = "${local.prefix}-p-monitor-rg"
+}
+
+
+variable "dev_subscription_name" {
+  type        = string
+  description = "DEV Subscription name"
+}
+
+variable "uat_subscription_name" {
+  type        = string
+  description = "UAT Subscription name"
+}
+
+variable "prod_subscription_name" {
+  type        = string
+  description = "PROD Subscription name"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name (e.g. pagoPA platform)"
+}
+
+variable "pipeline_environments" {
+  type        = list(any)
+  description = "List of environments pipeline to create"
+}
+
+variable "terraform_remote_state_app" {
+  type = object({
+    resource_group_name  = string,
+    storage_account_name = string,
+    container_name       = string,
+    key                  = string
+  })
 }
