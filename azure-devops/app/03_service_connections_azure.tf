04c43161befbad723d5b826de8ac79a48bf63131
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-16 16:29:44 +0100

    renamed service connection to the legacy name

--- azure-devops/app/03_service_connections_azure.tf
+++ azure-devops/app/03_service_connections_azure.tf
@@ -4,15 +4,14 @@
 
 module "DEV-AZURERM-SERVICE-CONN" {
   depends_on = [azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=fix-service-endpoint-federated-name"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.2"
   providers = {
     azurerm = azurerm.dev
   }
 
   project_id = azuredevops_project.project.id
   #tfsec:ignore:general-secrets-no-plaintext-exposure
-  name              = "${local.prefix}-${local.domain}-d-azdo-azurerm"
-  serviceendpoint_azurerm_name = "DEV-PAGOPA"
+  name              = "DEV-PAGOPA"
 
   tenant_id         = data.azurerm_client_config.current.tenant_id
   subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
@@ -33,15 +32,14 @@ resource "azurerm_role_assignment" "dev_azurerm" {
 #
 module "UAT-AZURERM-SERVICE-CONN" {
   depends_on = [azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=fix-service-endpoint-federated-name"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.2"
   providers = {
     azurerm = azurerm.uat
   }
 
   project_id = azuredevops_project.project.id
   #tfsec:ignore:general-secrets-no-plaintext-exposure
-  name              = "${local.prefix}-${local.domain}-u-azdo-azurerm"
-  serviceendpoint_azurerm_name = "UAT-PAGOPA"
+  name              = "UAT-PAGOPA"
 
   tenant_id         = data.azurerm_client_config.current.tenant_id
   subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
@@ -62,15 +60,14 @@ resource "azurerm_role_assignment" "uat_azurerm" {
 #
 module "PROD-AZURERM-SERVICE-CONN" {
   depends_on = [azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=fix-service-endpoint-federated-name"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.2"
   providers = {
     azurerm = azurerm.prod
   }
 
   project_id = azuredevops_project.project.id
   #tfsec:ignore:general-secrets-no-plaintext-exposure
-  name              = "${local.prefix}-${local.domain}-p-azdo-azurerm"
-  serviceendpoint_azurerm_name = "PROD-PAGOPA"
+  name              = "PROD-PAGOPA"
 
   tenant_id         = data.azurerm_client_config.current.tenant_id
   subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
