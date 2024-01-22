977d7c915552ad687f082c4b9f1fafc96aa22bd4
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-22 14:53:52 +0100

    added service connection managed identity for azure

--- azure-devops/iac/03_service_connections_azure.tf
+++ azure-devops/iac/03_service_connections_azure.tf
@@ -2,6 +2,25 @@
 # ⛩ Service connections
 #
 
+module "DEV-AZURERM-IAC-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.0.0"
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "AZDO-DEV-PAGOPA-IAC"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+  subscription_name = var.dev_subscription_name
+
+  location            = var.location
+  resource_group_name = local.dev_identity_rg_name
+}
+
 # 🟢 DEV service connection
 resource "azuredevops_serviceendpoint_azurerm" "DEV-SERVICE-CONN" {
   depends_on = [azuredevops_project.project]
@@ -14,6 +33,29 @@ resource "azuredevops_serviceendpoint_azurerm" "DEV-SERVICE-CONN" {
   azurerm_subscription_id   = module.secrets.values["DEV-SUBSCRIPTION-ID"].value
 }
 
+#
+# UAT
+#
+
+module "UAT-AZURERM-IAC-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.0.0"
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "AZDO-UAT-PAGOPA-IAC"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+  subscription_name = var.uat_subscription_name
+
+  location            = var.location
+  resource_group_name = local.uat_identity_rg_name
+}
+
 # 🟨 UAT service connection
 resource "azuredevops_serviceendpoint_azurerm" "UAT-SERVICE-CONN" {
   depends_on = [azuredevops_project.project]
@@ -24,6 +66,28 @@ resource "azuredevops_serviceendpoint_azurerm" "UAT-SERVICE-CONN" {
   azurerm_subscription_name = var.uat_subscription_name
   azurerm_spn_tenantid      = module.secrets.values["TENANTID"].value
   azurerm_subscription_id   = module.secrets.values["UAT-SUBSCRIPTION-ID"].value
+}
+
+#
+# PROD
+#
+module "PROD-AZURERM-IAC-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.0.0"
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "AZDO-PROD-PAGOPA-IAC"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+  subscription_name = var.prod_subscription_name
+
+  location            = var.location
+  resource_group_name = local.prod_identity_rg_name
 }
 
 # 🛑 PROD service connection
--- azure-devops/iac/99_variables.tf
+++ azure-devops/iac/99_variables.tf
@@ -1,62 +1,14 @@
-variable "dev_subscription_name" {
-  type        = string
-  description = "DEV Subscription name"
-}
-
-variable "uat_subscription_name" {
-  type        = string
-  description = "UAT Subscription name"
-}
-
-variable "prod_subscription_name" {
-  type        = string
-  description = "PROD Subscription name"
-}
-
-variable "project_name_prefix" {
-  type        = string
-  description = "Project name prefix (e.g. userregistry)"
-}
-
-variable "pipeline_environments" {
-  type        = list(any)
-  description = "List of environments pipeline to create"
-}
-
-variable "aks_dev_platform_name" {
-  type        = string
-  description = "AKS DEV platform name"
-}
-
-variable "aks_uat_platform_name" {
-  type        = string
-  description = "AKS UAT platform name"
-}
-
-variable "aks_prod_platform_name" {
-  type        = string
-  description = "AKS PROD platform name"
-}
-
-variable "apim_backup" {
-  default = {
-    repository = {
-      organization    = "pagopa"
-      name            = "pagopa-infra"
-      branch_name     = "refs/heads/main"
-      pipelines_path  = ".devops"
-      yml_prefix_name = "backup-apim"
-    }
-    pipeline = {
-      enable_code_review = false
-      enable_deploy      = true
-    }
-  }
-}
-
 locals {
   prefix           = "pagopa"
   azure_devops_org = "pagopaspa"
+
+  dev_subscription_name  = "dev-pagopa"
+  uat_subscription_name  = "uat-pagopa"
+  prod_subscription_name = "prod-pagopa"
+
+  dev_identity_rg_name  = "${local.prefix}-d-identity-rg"
+  uat_identity_rg_name  = "${local.prefix}-u-identity-rg"
+  prod_identity_rg_name = "${local.prefix}-p-identity-rg"
 
   # Service connections/ End points
   srv_endpoint_github_ro = "io-azure-devops-github-ro"
@@ -162,3 +114,60 @@ locals {
   prod_qi_key_vault_name = "${local.prefix}-p-qi-kv"
 
 }
+
+variable "dev_subscription_name" {
+  type        = string
+  description = "DEV Subscription name"
+}
+
+variable "uat_subscription_name" {
+  type        = string
+  description = "UAT Subscription name"
+}
+
+variable "prod_subscription_name" {
+  type        = string
+  description = "PROD Subscription name"
+}
+
+variable "project_name_prefix" {
+  type        = string
+  description = "Project name prefix (e.g. userregistry)"
+}
+
+variable "pipeline_environments" {
+  type        = list(any)
+  description = "List of environments pipeline to create"
+}
+
+variable "aks_dev_platform_name" {
+  type        = string
+  description = "AKS DEV platform name"
+}
+
+variable "aks_uat_platform_name" {
+  type        = string
+  description = "AKS UAT platform name"
+}
+
+variable "aks_prod_platform_name" {
+  type        = string
+  description = "AKS PROD platform name"
+}
+
+variable "apim_backup" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "pagopa-infra"
+      branch_name     = "refs/heads/main"
+      pipelines_path  = ".devops"
+      yml_prefix_name = "backup-apim"
+    }
+    pipeline = {
+      enable_code_review = false
+      enable_deploy      = true
+    }
+  }
+}
+
