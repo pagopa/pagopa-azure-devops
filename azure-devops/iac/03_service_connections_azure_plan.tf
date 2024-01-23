35375c5be3956c4634fa16d04e382fa1a99e77bb
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-23 10:19:31 +0100

    added azurerm plan service connection

---
+++ azure-devops/iac/03_service_connections_azure_plan.tf
@@ -0,0 +1,68 @@
+#
+# ⛩ Service connections
+#
+
+module "DEV-AZURERM-IAC-PLAN-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.0.0"
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "azdo-dev-pagopa-iac-plan"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+  subscription_name = var.dev_subscription_name
+
+  location            = var.location
+  resource_group_name = local.dev_identity_rg_name
+}
+
+#
+# UAT
+#
+
+module "UAT-AZURERM-IAC-PLAN-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.0.0"
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "azdo-uat-pagopa-iac-plan"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+  subscription_name = var.uat_subscription_name
+
+  location            = var.location
+  resource_group_name = local.uat_identity_rg_name
+}
+
+#
+# PROD
+#
+module "PROD-AZURERM-IAC-PLAN-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.0.0"
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  project_id = azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "azdo-prod-pagopa-iac-plan"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+  subscription_name = var.prod_subscription_name
+
+  location            = var.location
+  resource_group_name = local.prod_identity_rg_name
+}
+
