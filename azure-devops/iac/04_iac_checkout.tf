e5a5f1d187aacd72d2365acd6aede8378212d793
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-02-15 16:37:50 +0100

    fix checkout legacy pipeline

--- azure-devops/iac/04_iac_checkout.tf
+++ azure-devops/iac/04_iac_checkout.tf
@@ -1,4 +1,4 @@
-variable "checkout_iac" {
+variable "iac_checkout" {
   default = {
     repository = {
       organization    = "pagopa"
@@ -8,79 +8,75 @@ variable "checkout_iac" {
       yml_prefix_name = "checkout"
     }
     pipeline = {
-      enable_code_review   = true
-      enable_deploy        = true
-      path                 = "checkout-infrastructure"
-      pipeline_name_prefix = "checkout-infra"
+      enable_code_review = true
+      enable_deploy      = true
+      path_name          = "checkout-infra"
     }
   }
 }
 
 locals {
   # global vars
-  checkout_iac_variables = {
-    tf_dev_aks_apiserver_url         = module.aca_dev_secrets.values["pagopa-d-weu-dev-aks-apiserver-url"].value,
-    tf_dev_aks_azure_devops_sa_cacrt = module.aca_dev_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-cacrt"].value,
-    tf_dev_aks_azure_devops_sa_token = base64decode(module.aca_dev_secrets.values["pagopa-d-weu-dev-aks-azure-devops-sa-token"].value),
-    tf_aks_dev_name                  = var.aks_dev_platform_name
+  iac_checkout-variables = {
+    TF_POOL_NAME_DEV : "pagopa-dev-linux-infra",
+    TF_POOL_NAME_UAT : "pagopa-uat-linux-infra",
+    TF_POOL_NAME_PROD : "pagopa-prod-linux-infra",
 
-    tf_uat_aks_apiserver_url         = module.aca_uat_secrets.values["pagopa-u-weu-uat-aks-apiserver-url"].value,
-    tf_uat_aks_azure_devops_sa_cacrt = module.aca_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-cacrt"].value,
-    tf_uat_aks_azure_devops_sa_token = base64decode(module.aca_uat_secrets.values["pagopa-u-weu-uat-aks-azure-devops-sa-token"].value),
-    tf_aks_uat_name                  = var.aks_uat_platform_name
-
-    tf_prod_aks_apiserver_url         = module.aca_prod_secrets.values["pagopa-p-weu-prod-aks-apiserver-url"].value,
-    tf_prod_aks_azure_devops_sa_cacrt = module.aca_prod_secrets.values["pagopa-p-weu-prod-aks-azure-devops-sa-cacrt"].value,
-    tf_prod_aks_azure_devops_sa_token = base64decode(module.aca_prod_secrets.values["pagopa-p-weu-prod-aks-azure-devops-sa-token"].value),
-    tf_aks_prod_name                  = var.aks_prod_platform_name
-
-    TF_POOL_NAME_DEV  = "pagopa-dev-linux-infra",
-    TF_POOL_NAME_UAT  = "pagopa-uat-linux-infra",
-    TF_POOL_NAME_PROD = "pagopa-prod-linux-infra",
-    #PLAN
-    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_DEV  = module.DEV-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_name,
-    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_UAT  = module.UAT-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_name,
-    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_PROD = module.PROD-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_name,
-    #APPLY
-    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_DEV  = module.DEV-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
-    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_UAT  = module.UAT-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
-    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_PROD = module.PROD-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_DEV : module.DEV-PAGOPA-IAC-LEGACY-PLAN-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_UAT : module.UAT-PAGOPA-IAC-LEGACY-PLAN-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_PROD : module.PROD-PAGOPA-IAC-LEGACY-PLAN-SERVICE-CONN.service_endpoint_name,
   }
   # global secrets
-  checkout_iac_variables_secret = {}
+  iac_checkout-variables_secret = {
+
+  }
 
   # code_review vars
-  checkout_iac_variables_code_review = {}
+  iac_checkout-variables_code_review = {
+  }
   # code_review secrets
-  checkout_iac_variables_secret_code_review = {}
+  iac_checkout-variables_secret_code_review = {
+
+  }
 
   # deploy vars
-  checkout_iac_variables_deploy = {}
+  iac_checkout-variables_deploy = {
+    #APPLY
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_DEV : azuredevops_serviceendpoint_azurerm.DEV-PAGOPA-IAC-LEGACY.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_UAT : azuredevops_serviceendpoint_azurerm.UAT-PAGOPA-IAC-LEGACY.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_PROD : azuredevops_serviceendpoint_azurerm.PROD-PAGOPA-IAC-LEGACY.service_endpoint_name,
+
+  }
   # deploy secrets
-  checkout_iac_variables_secret_deploy = {}
+  iac_checkout-variables_secret_deploy = {
+
+  }
 }
 
-module "checkout_iac_code_review" {
+#
+# Code review
+#
+module "iac_checkout_code_review" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v6.0.0"
-  count  = var.checkout_iac.pipeline.enable_code_review == true ? 1 : 0
-  path   = var.checkout_iac.pipeline.path
+  count  = var.iac_checkout.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.checkout_iac.repository
+  repository                   = var.iac_checkout.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-pr.id
+  path                         = var.iac_checkout.pipeline.path_name
+  pipeline_name_prefix         = var.iac_checkout.repository.yml_prefix_name
 
-  pipeline_name_prefix = var.checkout_iac.pipeline.pipeline_name_prefix
 
   pull_request_trigger_use_yaml = true
 
   variables = merge(
-    local.checkout_iac_variables,
-    local.checkout_iac_variables_code_review,
+    local.iac_checkout-variables,
+    local.iac_checkout-variables_code_review,
   )
 
   variables_secret = merge(
-    local.checkout_iac_variables_secret,
-    local.checkout_iac_variables_secret_code_review,
+    local.iac_checkout-variables_secret,
+    local.iac_checkout-variables_secret_code_review,
   )
 
   service_connection_ids_authorization = [
@@ -91,38 +87,41 @@ module "checkout_iac_code_review" {
   ]
 }
 
-module "checkout_iac_deploy" {
+#
+# DEPLOY
+#
+module "iac_checkout_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v6.0.0"
-  count  = var.checkout_iac.pipeline.enable_deploy == true ? 1 : 0
-  path   = var.checkout_iac.pipeline.path
+  count  = var.iac_checkout.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.checkout_iac.repository
+  repository                   = var.iac_checkout.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-pr.id
-
-  pipeline_name_prefix = var.checkout_iac.pipeline.pipeline_name_prefix
+  path                         = var.iac_checkout.pipeline.path_name
+  pipeline_name_prefix         = var.iac_checkout.repository.yml_prefix_name
 
   ci_trigger_use_yaml           = false
   pull_request_trigger_use_yaml = false
 
+
   variables = merge(
-    local.checkout_iac_variables,
-    local.checkout_iac_variables_deploy,
+    local.iac_checkout-variables,
+    local.iac_checkout-variables_deploy,
   )
 
   variables_secret = merge(
-    local.checkout_iac_variables_secret,
-    local.checkout_iac_variables_secret_deploy,
+    local.iac_checkout-variables_secret,
+    local.iac_checkout-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
     azuredevops_serviceendpoint_github.azure-devops-github-ro.id,
-    module.DEV-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
-    module.UAT-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
-    module.PROD-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
+    module.DEV-PAGOPA-IAC-LEGACY-PLAN-SERVICE-CONN.service_endpoint_id,
+    module.UAT-PAGOPA-IAC-LEGACY-PLAN-SERVICE-CONN.service_endpoint_id,
+    module.PROD-PAGOPA-IAC-LEGACY-PLAN-SERVICE-CONN.service_endpoint_id,
 
-    module.DEV-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
-    module.UAT-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
-    module.PROD-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
+    azuredevops_serviceendpoint_azurerm.DEV-PAGOPA-IAC-LEGACY.id,
+    azuredevops_serviceendpoint_azurerm.UAT-PAGOPA-IAC-LEGACY.id,
+    azuredevops_serviceendpoint_azurerm.PROD-PAGOPA-IAC-LEGACY.id,
   ]
 }
