1daa1447c4aa97113d2c27203d871e59c2a67547
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-02-16 15:12:41 +0100

    added canoneunico pipeline

---
+++ azure-devops/iac/04_iac_canoneunico.tf
@@ -0,0 +1,113 @@
+variable "canoneunico_iac" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "pagopa-infra"
+      branch_name     = "refs/heads/main"
+      pipelines_path  = ".devops"
+      yml_prefix_name = "canoneunico"
+    }
+    pipeline = {
+      enable_code_review   = true
+      enable_deploy        = true
+      path                 = "canoneunico-infrastructure"
+      pipeline_name_prefix = "canoneunico-infra"
+    }
+  }
+}
+
+locals {
+  # global vars
+  canoneunico_iac_variables = {
+    TF_POOL_NAME_DEV  = "pagopa-dev-linux-infra",
+    TF_POOL_NAME_UAT  = "pagopa-uat-linux-infra",
+    TF_POOL_NAME_PROD = "pagopa-prod-linux-infra",
+    #PLAN
+    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_DEV  = module.DEV-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_UAT  = module.UAT-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_PLAN_NAME_PROD = module.PROD-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_name,
+    #APPLY
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_DEV  = module.DEV-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_UAT  = module.UAT-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_PROD = module.PROD-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
+  }
+  # global secrets
+  canoneunico_iac_variables_secret = {}
+
+  # code_review vars
+  canoneunico_iac_variables_code_review = {}
+  # code_review secrets
+  canoneunico_iac_variables_secret_code_review = {}
+
+  # deploy vars
+  canoneunico_iac_variables_deploy = {}
+  # deploy secrets
+  canoneunico_iac_variables_secret_deploy = {}
+}
+
+module "canoneunico_iac_code_review" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v6.0.0"
+  count  = var.canoneunico_iac.pipeline.enable_code_review == true ? 1 : 0
+  path   = var.canoneunico_iac.pipeline.path
+
+  project_id                   = azuredevops_project.project.id
+  repository                   = var.canoneunico_iac.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-pr.id
+
+  pipeline_name_prefix = var.canoneunico_iac.pipeline.pipeline_name_prefix
+
+  pull_request_trigger_use_yaml = true
+
+  variables = merge(
+    local.canoneunico_iac_variables,
+    local.canoneunico_iac_variables_code_review,
+  )
+
+  variables_secret = merge(
+    local.canoneunico_iac_variables_secret,
+    local.canoneunico_iac_variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.azure-devops-github-ro.id,
+    module.DEV-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
+    module.UAT-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
+    module.PROD-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
+  ]
+}
+
+module "canoneunico_iac_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v6.0.0"
+  count  = var.canoneunico_iac.pipeline.enable_deploy == true ? 1 : 0
+  path   = var.canoneunico_iac.pipeline.path
+
+  project_id                   = azuredevops_project.project.id
+  repository                   = var.canoneunico_iac.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-pr.id
+
+  pipeline_name_prefix = var.canoneunico_iac.pipeline.pipeline_name_prefix
+
+  ci_trigger_use_yaml           = false
+  pull_request_trigger_use_yaml = false
+
+  variables = merge(
+    local.canoneunico_iac_variables,
+    local.canoneunico_iac_variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.canoneunico_iac_variables_secret,
+    local.canoneunico_iac_variables_secret_deploy,
+  )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.azure-devops-github-ro.id,
+    module.DEV-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
+    module.UAT-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
+    module.PROD-AZURERM-IAC-PLAN-SERVICE-CONN.service_endpoint_id,
+
+    module.DEV-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
+    module.UAT-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
+    module.PROD-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
+  ]
+}
