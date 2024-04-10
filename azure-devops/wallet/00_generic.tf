1cb09f9ca717cf7e614c18236f513563a7bf55c3
Giovanni Berti <giovanni.berti@pagopa.it>
2024-04-10 16:27:33 +0200

    feat(wallet): Add pipelines for `payment-wallet-event-dispatcher` (#323)

--- azure-devops/wallet/00_generic.tf
+++ azure-devops/wallet/00_generic.tf
@@ -11,4 +11,8 @@ data "azurerm_subscriptions" "prod" {
   display_name_prefix = local.prod_subscription_name
 }
 
-
+resource "azurerm_role_assignment" "aks_to_acr" {
+  scope                = module.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = module.aks.kubelet_identity_id
+}
