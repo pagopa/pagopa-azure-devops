6fe56b516c2b30225162d788e0b1b2ed3b741f1f
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-04-12 10:56:55 +0200

    fix pipeline

--- azure-devops/iac/06_disaster_recovery.tf
+++ azure-devops/iac/06_disaster_recovery.tf
@@ -5,16 +5,26 @@ module "disaster_recovery" {
   repository                   = var.disaster_recovery.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-pr.id
   path                         = "disaster-recovery"
-  pipeline_name                = "postgres-failover"
-  pipeline_yml_filename        = "postgres-failover.yml"
+  pipeline_name                = "disaster-recovery"
+  pipeline_yml_filename        = "disaster-recovery.yml"
 
   ci_trigger_use_yaml = false
 
-  variables = {}
+  variables = {
+
+    TF_POOL_NAME_DEV  = "pagopa-dev-linux-infra",
+    TF_POOL_NAME_UAT  = "pagopa-uat-linux-infra",
+    TF_POOL_NAME_PROD = "pagopa-prod-linux-infra",
+    #APPLY
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_DEV  = module.DEV-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_UAT  = module.UAT-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
+    TF_AZURE_SERVICE_CONNECTION_APPLY_NAME_PROD = module.PROD-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_name,
+  }
 
   variables_secret = {}
 
   service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.azure-devops-github-ro.id,
     module.DEV-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
     module.UAT-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
     module.PROD-AZURERM-IAC-DEPLOY-SERVICE-CONN.service_endpoint_id,
