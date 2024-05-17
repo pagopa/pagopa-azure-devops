b55386ba5178e20cfdde4724e3b7422dc9bb20c2
Simone Infante <simone.infante@pagopa.it>
2024-05-17 14:36:41 +0200

    fix: cr name

--- azure-devops/pay-wallet/00_service_connections.tf
+++ azure-devops/pay-wallet/00_service_connections.tf
@@ -39,7 +39,7 @@ data "azuredevops_serviceendpoint_azurerm" "dev" {
 #
 data "azuredevops_serviceendpoint_azurecr" "dev" {
   project_id            = data.azuredevops_project.project.id
-  service_endpoint_name = var.service_connection_dev_acr_name
+  service_endpoint_name = var.srv_endpoint_name_docker_registry_italy_dev
 }
 
 # data "azuredevops_serviceendpoint_azurecr" "uat" {
