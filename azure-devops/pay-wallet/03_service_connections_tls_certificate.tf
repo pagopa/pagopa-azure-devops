af8e6730748299fc2d6fdf984269f1e822394b86
Simone infante <52280205+infantesimone@users.noreply.github.com>
2024-06-11 18:58:34 +0200

    feat(pay-wallet): CHK-1962 Add new domain for wallet (#329)
    * feat: add new domain for wallet
    
    * fir(format)
    
    * fix: pay-wallet state file
    
    * feat: add tf lock
    
    * fix: kv pay-wallet name
    
    * fix: rg for pay-wallet kv
    
    * fix: disable uat env
    
    * format
    
    * fix: container namespace
    
    * fix: cr name
    
    * fix: tmp acr sc
    
    * feat: add tls internal for dev
    
    * fix: tls pipeline
    
    * fix: kv for internal cert
    
    * fix: cert name for pay wallet
    
    * fix(event-dispatcher): pipeline conf
    
    * fix: prefix pipeline
    
    * feat: add uat config
    
    * fix: service connection acr
    
    * feat: add pipeline for internal dns
    
    * feat: add config for event dispatcher
    
    * fix: domain

---
+++ azure-devops/pay-wallet/03_service_connections_tls_certificate.tf
@@ -0,0 +1,87 @@
+#
+# DEV
+#
+module "DEV-WALLET-TLS-CERT-SERVICE-CONN" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.1"
+
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-${local.domain}-d-tls-cert-azdo"
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = var.dev_subscription_name
+  subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+
+  location            = var.location
+  resource_group_name = local.dev_identity_rg_name
+}
+
+resource "azurerm_key_vault_access_policy" "DEV-WALLET-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.dev
+  key_vault_id = data.azurerm_key_vault.domain_kv_dev.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.DEV-WALLET-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
+
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_dev" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+  prefix            = local.prefix
+  env               = "d"
+  key_vault_name    = local.dev_wallet_key_vault_name
+  subscription_name = var.dev_subscription_name
+}
+
+#
+# UAT
+#
+
+module "UAT-WALLET-TLS-CERT-SERVICE-CONN" {
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.1"
+
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-${local.domain}-u-tls-cert-azdo"
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = var.uat_subscription_name
+  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+
+  location            = var.location
+  resource_group_name = local.uat_identity_rg_name
+}
+
+resource "azurerm_key_vault_access_policy" "UAT-WALLET-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.uat
+  key_vault_id = data.azurerm_key_vault.domain_kv_uat.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.UAT-WALLET-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
+
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_uat" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
+
+  providers = {
+    azurerm = azurerm.uat
+  }
+  prefix            = local.prefix
+  env               = "u"
+  key_vault_name    = local.uat_wallet_key_vault_name
+  subscription_name = var.uat_subscription_name
+}
---
+++ azure-devops/pay-wallet/99_main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 1.3.5"
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 0.10.0"
+    }
+    azurerm = {
+      version = "<= 3.85.0"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+  alias           = "dev"
+  subscription_id = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+  alias           = "uat"
+  subscription_id = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+  alias           = "prod"
+  subscription_id = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+}
