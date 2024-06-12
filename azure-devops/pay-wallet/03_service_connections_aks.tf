51cfef47c558ccf842fd4d2da1eac70478c1c544
Simone Infante <simone.infante@pagopa.it>
2024-06-12 14:32:04 +0200

    feat: enable prod pipelien for pay-wallet

--- azure-devops/pay-wallet/03_service_connections_aks.tf
+++ azure-devops/pay-wallet/03_service_connections_aks.tf
@@ -23,3 +23,16 @@ resource "azuredevops_serviceendpoint_kubernetes" "aks_uat" {
     ca_cert = module.wallet_uat_secrets.values["pagopa-u-itn-uat-aks-azure-devops-sa-cacrt"].value
   }
 }
+
+resource "azuredevops_serviceendpoint_kubernetes" "aks_prod" {
+  depends_on            = [data.azuredevops_project.project]
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = local.srv_endpoint_name_aks_prod
+  apiserver_url         = module.wallet_prod_secrets.values["pagopa-p-itn-prod-aks-apiserver-url"].value
+  authorization_type    = "ServiceAccount"
+  service_account {
+    # base64 values
+    token   = module.wallet_prod_secrets.values["pagopa-p-itn-prod-aks-azure-devops-sa-token"].value
+    ca_cert = module.wallet_prod_secrets.values["pagopa-p-itn-prod-aks-azure-devops-sa-cacrt"].value
+  }
+}
