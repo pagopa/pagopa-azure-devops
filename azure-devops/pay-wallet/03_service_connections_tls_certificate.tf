ee4a640a1f324c8882ba9b18fe4c029ed6bcfaaa
Simone Infante <simone.infante@pagopa.it>
2024-06-14 13:15:49 +0200

    feat: add prod internal dns for pay-wallet

--- azure-devops/pay-wallet/03_service_connections_tls_certificate.tf
+++ azure-devops/pay-wallet/03_service_connections_tls_certificate.tf
@@ -85,3 +85,47 @@ module "letsencrypt_uat" {
   key_vault_name    = local.uat_wallet_key_vault_name
   subscription_name = var.uat_subscription_name
 }
+
+#
+# PROD
+#
+
+module "PROD-WALLET-TLS-CERT-SERVICE-CONN" {
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.1"
+
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-${local.domain}-p-tls-cert-azdo"
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = var.prod_subscription_name
+  subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+
+  location            = var.location
+  resource_group_name = local.prod_identity_rg_name
+}
+
+resource "azurerm_key_vault_access_policy" "PROD-WALLET-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.prod
+  key_vault_id = data.azurerm_key_vault.domain_kv_prod.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.PROD-WALLET-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
+
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_prod" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
+
+  providers = {
+    azurerm = azurerm.prod
+  }
+  prefix            = local.prefix
+  env               = "p"
+  key_vault_name    = local.prod_wallet_key_vault_name
+  subscription_name = var.prod_subscription_name
+}
