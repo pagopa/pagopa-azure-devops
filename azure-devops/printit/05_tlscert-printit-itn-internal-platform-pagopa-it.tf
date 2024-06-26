704eaa4c7ccd47f6ba1df95ee340f858d579bf8e
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-06-26 14:59:40 +0200

    updated modules, fix variables and minor fix

--- azure-devops/printit/05_tlscert-printit-itn-internal-platform-pagopa-it.tf
+++ azure-devops/printit/05_tlscert-printit-itn-internal-platform-pagopa-it.tf
@@ -8,10 +8,10 @@ variable "tlscert-printit-itn-internal-prod-platform-pagopa-it" {
    }
    pipeline = {
      enable_tls_cert         = true
-     path                    = "TLS-Certificates\\DEV"
+     path                    = "TLS-Certificates\\PROD"
      dns_record_name         = "printit.itn.internal"
-     dns_zone_name           = "dev.platform.pagopa.it"
-     dns_zone_resource_group = "pagopa-p-itn-printit-itn-sec-rg"
+     dns_zone_name           = "platform.pagopa.it"
+     dns_zone_resource_group = "pagopa-p-vnet-rg"
      # common variables to all pipelines
      variables = {
      }
@@ -26,7 +26,7 @@ locals {
  tlscert-printit-itn-internal-prod-platform-pagopa-it = {
    tenant_id         = data.azurerm_client_config.current.tenant_id
    subscription_name = "PROD-PAGOPA"
-   subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+   subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
  }
  tlscert-printit-itn-internal-prod-platform-pagopa-it-variables = {
    KEY_VAULT_SERVICE_CONNECTION = module.PROD-PRINTIT-TLS-CERT-SERVICE-CONN.service_endpoint_name
@@ -38,7 +38,7 @@ locals {
 module "tlscert-printit-itn-internal-prod-platform-pagopa-it-cert_az" {
 
  providers = {
-   azurerm = azurerm.dev
+   azurerm = azurerm.prod
  }
 
  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert_federated?ref=v9.0.0"
@@ -56,9 +56,9 @@ module "tlscert-printit-itn-internal-prod-platform-pagopa-it-cert_az" {
  subscription_name                    = local.tlscert-printit-itn-internal-prod-platform-pagopa-it.subscription_name
  subscription_id                      = local.tlscert-printit-itn-internal-prod-platform-pagopa-it.subscription_id
  location                             = local.location_westeurope
- credential_key_vault_name            = local.dev_printit-itn_key_vault_name
- credential_key_vault_resource_group  = local.dev_printit-itn_key_vault_resource_group
- managed_identity_resource_group_name = local.dev_identity_rg_name
+ credential_key_vault_name            = local.prod_printit_key_vault_name
+ credential_key_vault_resource_group  = local.prod_printit_key_vault_resource_group
+ managed_identity_resource_group_name = local.prod_identity_rg_name
 
  variables = merge(
    var.tlscert-printit-itn-internal-prod-platform-pagopa-it.pipeline.variables,
