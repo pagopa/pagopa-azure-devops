69240cbc1c783621f372971441347a38487285b1
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-27 16:55:00 +0200

    added workload identity, in core

--- azure-devops/pay-wallet/00_service_connections.tf
+++ azure-devops/pay-wallet/00_service_connections.tf
@@ -39,17 +39,17 @@ data "azuredevops_serviceendpoint_azurerm" "prod" {
 #
 data "azuredevops_serviceendpoint_azurecr" "dev_ita_workload_identity" {
   project_id            = data.azuredevops_project.project.id
-  service_endpoint_name = "docker-registry-ita-dev-workload-identity"
+  service_endpoint_name = var.acr_ita_service_connection_workload_identity_dev
 }
 
 data "azuredevops_serviceendpoint_azurecr" "uat_ita_workload_identity" {
   project_id            = data.azuredevops_project.project.id
-  service_endpoint_name = "docker-registry-ita-uat-workload-identity"
+  service_endpoint_name = var.acr_ita_service_connection_workload_identity_uat
 }
 
 data "azuredevops_serviceendpoint_azurecr" "prod_ita_workload_identity" {
   project_id            = data.azuredevops_project.project.id
-  service_endpoint_name = "docker-registry-ita-prod-workload-identity"
+  service_endpoint_name = var.acr_ita_service_connection_workload_identity_prod
 }
 
 #
