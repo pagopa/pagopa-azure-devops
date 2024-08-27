9a7c955b314adce467a89430bd13775ec89fbb6d
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-27 12:05:34 +0200

    added acr legacy backup

--- azure-devops/app/03_service_connections_docker_legacy_backup.tf
+++ azure-devops/app/03_service_connections_docker_legacy_backup.tf
@@ -3,25 +3,27 @@
 #
 
 # 🟢 DEV service connection for azure container registry
-resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_dev" {
+resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_dev_legacy_backup" {
   depends_on = [azuredevops_project.project]
 
   project_id     = azuredevops_project.project.id
   resource_group = local.docker_registry_rg_name_dev
 
-  service_endpoint_name = local.srv_endpoint_name_docker_registry_dev
+  service_endpoint_name = "${local.srv_endpoint_name_docker_registry_dev}_legacy_backup"
   azurecr_name          = local.docker_registry_name_dev
 
   azurecr_subscription_name = var.dev_subscription_name
   azurecr_spn_tenantid      = data.azurerm_client_config.current.tenant_id
   azurecr_subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+
+  description = "Managed by terraform - Created on ${formatdate("YYYY-MM-DD", timestamp())}"
 }
 
 # 🟨 UAT service connection for azure container registry
-resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_uat" {
+resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_uat_legacy_backup" {
   depends_on = [azuredevops_project.project]
 
-  service_endpoint_name = local.srv_endpoint_name_docker_registry_uat
+  service_endpoint_name = "${local.srv_endpoint_name_docker_registry_uat}_legacy_backup"
   azurecr_name          = local.docker_registry_name_uat
 
   project_id     = azuredevops_project.project.id
@@ -30,13 +32,15 @@ resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_uat" {
   azurecr_subscription_name = var.uat_subscription_name
   azurecr_spn_tenantid      = data.azurerm_client_config.current.tenant_id
   azurecr_subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+
+  description = "Managed by terraform - Created on ${formatdate("YYYY-MM-DD", timestamp())}"
 }
 
 # 🛑 PROD service connection for azure container registry
-resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_prod" {
+resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_prod_legacy_backup" {
   depends_on = [azuredevops_project.project]
 
-  service_endpoint_name = local.srv_endpoint_name_docker_registry_prod
+  service_endpoint_name = "${local.srv_endpoint_name_docker_registry_prod}_legacy_backup"
   azurecr_name          = local.docker_registry_name_prod
 
   project_id     = azuredevops_project.project.id
@@ -45,6 +49,8 @@ resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_prod" {
   azurecr_subscription_name = var.prod_subscription_name
   azurecr_spn_tenantid      = data.azurerm_client_config.current.tenant_id
   azurecr_subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+
+  description = "Managed by terraform - Created on ${formatdate("YYYY-MM-DD", timestamp())}"
 }
 
 #
@@ -52,46 +58,52 @@ resource "azuredevops_serviceendpoint_azurecr" "acr_docker_registry_prod" {
 #
 
 # 🟢 DEV service connection for AKS container registry in italy north
-resource "azuredevops_serviceendpoint_azurecr" "aks_cr_itn_dev" {
+resource "azuredevops_serviceendpoint_azurecr" "aks_cr_itn_dev_legacy_backup" {
   depends_on = [azuredevops_project.project]
 
   project_id     = azuredevops_project.project.id
   resource_group = local.docker_registry_italy_rg_name_dev
 
-  service_endpoint_name = local.srv_endpoint_name_docker_registry_italy_dev
+  service_endpoint_name = "${local.srv_endpoint_name_docker_registry_italy_dev}_legacy_backup"
   azurecr_name          = local.docker_registry_italy_name_dev
 
   azurecr_subscription_name = var.dev_subscription_name
   azurecr_spn_tenantid      = data.azurerm_client_config.current.tenant_id
   azurecr_subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+
+  description = "Managed by terraform - Created on ${formatdate("YYYY-MM-DD", timestamp())}"
 }
 
 # 🟨 UAT service connection for azure container registry in italy north
-resource "azuredevops_serviceendpoint_azurecr" "aks_cr_itn_uat" {
+resource "azuredevops_serviceendpoint_azurecr" "aks_cr_itn_uat_legacy_backup" {
   depends_on = [azuredevops_project.project]
 
   project_id     = azuredevops_project.project.id
   resource_group = local.docker_registry_italy_rg_name_uat
 
-  service_endpoint_name = local.srv_endpoint_name_docker_registry_italy_uat
+  service_endpoint_name = "${local.srv_endpoint_name_docker_registry_italy_uat}_legacy_backup"
   azurecr_name          = local.docker_registry_italy_name_uat
 
   azurecr_subscription_name = var.uat_subscription_name
   azurecr_spn_tenantid      = data.azurerm_client_config.current.tenant_id
   azurecr_subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+
+  description = "Managed by terraform - Created on ${formatdate("YYYY-MM-DD", timestamp())}"
 }
 
 # 🛑 PROD service connection for azure container registry in italy north
-resource "azuredevops_serviceendpoint_azurecr" "aks_cr_itn_prod" {
+resource "azuredevops_serviceendpoint_azurecr" "aks_cr_itn_prod_legacy_backup" {
   depends_on = [azuredevops_project.project]
 
   project_id     = azuredevops_project.project.id
   resource_group = local.docker_registry_italy_rg_name_prod
 
-  service_endpoint_name = local.srv_endpoint_name_docker_registry_italy_prod
+  service_endpoint_name = "${local.srv_endpoint_name_docker_registry_italy_prod}_legacy_backup"
   azurecr_name          = local.docker_registry_italy_name_prod
 
   azurecr_subscription_name = var.prod_subscription_name
   azurecr_spn_tenantid      = data.azurerm_client_config.current.tenant_id
   azurecr_subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+
+  description = "Managed by terraform - Created on ${formatdate("YYYY-MM-DD", timestamp())}"
 }
