9e72d378f148dd2c0fea2b1b4f73155c21a9020c
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2024-09-24 16:58:57 +0200

    fix

--- azure-devops/paymentoptions/05_tlscert-payopt-itn-internal-dev-platform-pagopa-it.tf
+++ azure-devops/paymentoptions/05_tlscert-payopt-itn-internal-dev-platform-pagopa-it.tf
@@ -9,7 +9,7 @@ variable "tlscert-payopt-itn-internal-dev-platform-pagopa-it" {
     pipeline = {
       enable_tls_cert         = true
       path                    = "TLS-Certificates\\DEV"
-      dns_record_name         = "paymentoptions.itn.internal"
+      dns_record_name         = "payopt.itn.internal"
       dns_zone_name           = "dev.platform.pagopa.it"
       dns_zone_resource_group = "pagopa-d-vnet-rg"
       # common variables to all pipelines
