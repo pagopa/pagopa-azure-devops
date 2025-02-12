eda78c01571b0a506a6cc5b12e698db920a75428
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-02-12 13:36:49 +0100

    feat: add uat pipeline for ingress uat checkout

--- azure-devops/checkout/03_service_connections_tls_certificate.tf
+++ azure-devops/checkout/03_service_connections_tls_certificate.tf
@@ -46,45 +46,45 @@ module "letsencrypt_dev" {
 # UAT
 #
 
-# module "UAT-CHECKOUT-TLS-CERT-SERVICE-CONN" {
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
+module "UAT-CHECKOUT-TLS-CERT-SERVICE-CONN" {
+  providers = {
+    azurerm = azurerm.uat
+  }
 
-#   depends_on = [data.azuredevops_project.project]
-#   source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.1"
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v4.2.1"
 
-#   project_id        = data.azuredevops_project.project.id
-#   name              = "${local.prefix}-${local.domain}-u-tls-cert-azdo"
-#   tenant_id         = data.azurerm_client_config.current.tenant_id
-#   subscription_name = var.uat_subscription_name
-#   subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-${local.domain}-u-tls-cert-azdo"
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = var.uat_subscription_name
+  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
 
-#   location            = local.location
-#   resource_group_name = local.uat_identity_rg_name
-# }
+  location            = local.location
+  resource_group_name = local.uat_identity_rg_name
+}
 
-# resource "azurerm_key_vault_access_policy" "UAT-CHECKOUT-TLS-CERT-SERVICE-CONN_kv_access_policy" {
-#   provider     = azurerm.uat
-#   key_vault_id = data.azurerm_key_vault.domain_kv_uat.id
-#   tenant_id    = data.azurerm_client_config.current.tenant_id
-#   object_id    = module.UAT-CHECKOUT-TLS-CERT-SERVICE-CONN.service_principal_object_id
+resource "azurerm_key_vault_access_policy" "UAT-CHECKOUT-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.uat
+  key_vault_id = data.azurerm_key_vault.domain_kv_uat.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.UAT-CHECKOUT-TLS-CERT-SERVICE-CONN.service_principal_object_id
 
-#   certificate_permissions = ["Get", "Import"]
-# }
+  certificate_permissions = ["Get", "Import"]
+}
 
-# # create let's encrypt credential used to create SSL certificates
-# module "letsencrypt_uat" {
-#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_uat" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
 
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
-#   prefix            = local.prefix
-#   env               = "u"
-#   key_vault_name    = local.uat_checkout_key_vault_name
-#   subscription_name = var.uat_subscription_name
-# }
+  providers = {
+    azurerm = azurerm.uat
+  }
+  prefix            = local.prefix
+  env               = "u"
+  key_vault_name    = local.uat_checkout_key_vault_name
+  subscription_name = var.uat_subscription_name
+}
 
 # #
 # # PROD
