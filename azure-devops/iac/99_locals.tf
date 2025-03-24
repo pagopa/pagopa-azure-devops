2415992dac144efdf3462b5ed86e89e9c4e1ad5f
Marco Mari <130982006+mamari90@users.noreply.github.com>
2025-03-24 10:29:45 +0100

    feat: Added generic pipeline handling (#407)
    * added generic pipeline handling
    
    * precommit

--- azure-devops/iac/99_locals.tf
+++ azure-devops/iac/99_locals.tf
@@ -29,4 +29,32 @@ locals {
   ]
 
   definitions_variables = {}
+
+  generic_pipelines = {
+    "gh-runner-daily-cleanup" : {
+      pipeline_prefix : "gh-runner-daily-cleanup",
+      pipeline_path : "gh-runner-cleanup",
+      repository : {
+        yml_file_name : "gh-runner-cleanup.yml"
+      }
+      schedules : {
+        days_to_build : ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        schedule_only_with_changes : false,
+        start_hours : 18,
+        start_minutes : 0,
+        time_zone : "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna",
+        branch_filter : {
+          include : ["refs/heads/main"],
+          exclude : []
+        }
+      }
+    },
+    "disaster-recovery" : {
+      pipeline_prefix : "disaster-recovery",
+      pipeline_path : "disaster-recovery",
+      repository : {
+        yml_file_name : "disaster-recovery.yml"
+      }
+    }
+  }
 }
