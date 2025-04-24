41946a1d6104d190436a95aa3f0e653d09b1eb32
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2025-04-24 14:29:00 +0200

    feat: Add cert `crusc8-itn-internal-dev-platform-pagopa-it` (#414)
    * add-crusc8-cert-dev
    
    * minor fix

---
+++ azure-devops/crusc8/00_secrets_crusc8.tf
@@ -0,0 +1,101 @@
+module "crusc8_dev_secrets" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "./.terraform/modules/__v3__/key_vault_secrets_query"
+
+  resource_group = local.dev_crusc8_key_vault_resource_group
+  key_vault_name = local.dev_crusc8_key_vault_name
+
+  secrets = [
+    "pagopa-d-itn-dev-aks-azure-devops-sa-token",
+    "pagopa-d-itn-dev-aks-azure-devops-sa-cacrt",
+    "pagopa-d-itn-dev-aks-apiserver-url"
+  ]
+}
+
+# module "crusc8_uat_secrets" {
+
+#   providers = {
+#     azurerm = azurerm.uat
+#   }
+
+#   source = "./.terraform/modules/__v3__/key_vault_secrets_query"
+
+#   resource_group = local.uat_crusc8_key_vault_resource_group
+#   key_vault_name = local.uat_crusc8_key_vault_name
+
+#   secrets = [
+#     "pagopa-u-itn-uat-aks-azure-devops-sa-token",
+#     "pagopa-u-itn-uat-aks-azure-devops-sa-cacrt",
+#     "pagopa-u-itn-uat-aks-apiserver-url",
+#     "apikey-gps-mbd-integration-test",
+#     "apikey-mbd-integration-test"
+#   ]
+# }
+
+# module "crusc8_prod_secrets" {
+
+#   providers = {
+#     azurerm = azurerm.prod
+#   }
+
+#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.22.0"
+
+#   resource_group = local.prod_crusc8_key_vault_resource_group
+#   key_vault_name = local.prod_crusc8_key_vault_name
+
+#   secrets = [
+#     "pagopa-p-itn-prod-aks-azure-devops-sa-token",
+#     "pagopa-p-itn-prod-aks-azure-devops-sa-cacrt",
+#     "pagopa-p-itn-prod-aks-apiserver-url",
+#   ]
+# }
+
+
+module "general_dev_secrets" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.22.0"
+
+  resource_group = "pagopa-d-sec-rg"
+  key_vault_name = "pagopa-d-kv"
+
+  secrets = []
+}
+
+# module "general_uat_secrets" {
+
+#   providers = {
+#     azurerm = azurerm.uat
+#   }
+
+#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.22.0"
+
+#   resource_group = "pagopa-u-sec-rg"
+#   key_vault_name = "pagopa-u-kv"
+
+#   secrets = [
+#     "integration-test-subkey",
+#   ]
+# }
+
+# module "general_prod_secrets" {
+
+#   providers = {
+#     azurerm = azurerm.prod
+#   }
+
+#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.22.0"
+
+#   resource_group = "pagopa-p-sec-rg"
+#   key_vault_name = "pagopa-p-kv"
+
+#   secrets = [
+#   ]
+# }
