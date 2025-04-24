6a60e69afd61e14a5b6e43788ed850832bf6e1a7
pasqualespica <pasqualespica@gmail.com>
2025-04-24 13:50:00 +0200

    add-crusc8-cert-dev

---
+++ azure-devops/crusc8/00_service_connections.tf
@@ -0,0 +1,35 @@
+#
+# GITHUB
+#
+data "azuredevops_serviceendpoint_github" "github_pr" {
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-pr"
+}
+
+data "azuredevops_serviceendpoint_github" "github_ro" {
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-ro"
+}
+
+data "azuredevops_serviceendpoint_github" "github_rw" {
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-rw"
+}
+
+#
+# AZURERM
+#
+data "azuredevops_serviceendpoint_azurerm" "dev" {
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = var.service_connection_dev_azurerm_name
+}
+
+# data "azuredevops_serviceendpoint_azurerm" "uat" {
+#   project_id            = data.azuredevops_project.project.id
+#   service_endpoint_name = var.service_connection_uat_azurerm_name
+# }
+
+# data "azuredevops_serviceendpoint_azurerm" "prod" {
+#   project_id            = data.azuredevops_project.project.id
+#   service_endpoint_name = var.service_connection_prod_azurerm_name
+# }
