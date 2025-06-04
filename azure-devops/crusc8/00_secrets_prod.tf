ba1d8e51273bb7323cd675f959d3beb4324637cf
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2025-06-04 08:02:18 +0200

    feat: Add pipeline uat cert cruscotto (#426)
    add pipi uat cert cruscotto

--- azure-devops/crusc8/00_secrets_prod.tf
+++ azure-devops/crusc8/00_secrets_prod.tf
@@ -22,9 +22,9 @@ module "secrets" {
 #   display_name_prefix = local.prod_subscription_name
 # }
 
-# data "azurerm_subscriptions" "uat" {
-#   display_name_prefix = local.uat_subscription_name
-# }
+data "azurerm_subscriptions" "uat" {
+  display_name_prefix = local.uat_subscription_name
+}
 
 data "azurerm_subscriptions" "dev" {
   display_name_prefix = local.dev_subscription_name
--- azure-devops/crusc8/05_tlscert-crusc8-uat-platform-pagopa-it.tf
+++ azure-devops/crusc8/05_tlscert-crusc8-uat-platform-pagopa-it.tf
@@ -1,4 +1,4 @@
-variable "tlscert-crusc8-itn-internal-uat-platform-pagopa-it" {
+variable "tlscert-crusc8-uat-platform-pagopa-it" {
   default = {
     repository = {
       organization   = "pagopa"
@@ -9,7 +9,7 @@ variable "tlscert-crusc8-itn-internal-uat-platform-pagopa-it" {
     pipeline = {
       enable_tls_cert         = true
       path                    = "TLS-Certificates\\UAT"
-      dns_record_name         = "crusc8.itn.internal"
+      dns_record_name         = "crusc8"
       dns_zone_name           = "uat.platform.pagopa.it"
       dns_zone_resource_group = "pagopa-u-vnet-rg"
       # common variables to all pipelines
@@ -25,51 +25,52 @@ variable "tlscert-crusc8-itn-internal-uat-platform-pagopa-it" {
 }
 
 locals {
-  tlscert-crusc8-itn-internal-uat-platform-pagopa-it = {
+  tlscert-crusc8-uat-platform-pagopa-it = {
     tenant_id         = data.azurerm_client_config.current.tenant_id
     subscription_name = "UAT-PAGOPA"
     subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
   }
-  tlscert-crusc8-itn-internal-uat-platform-pagopa-it-variables = {
+  tlscert-crusc8-uat-platform-pagopa-it-variables = {
     KEY_VAULT_SERVICE_CONNECTION = module.UAT-CRUSC8-TLS-CERT-SERVICE-CONN.service_endpoint_name
   }
-  tlscert-crusc8-itn-internal-uat-platform-pagopa-it-variables_secret = {
+  tlscert-crusc8-uat-platform-pagopa-it-variables_secret = {
   }
 }
 
-module "tlscert-crusc8-itn-internal-uat-platform-pagopa-it-cert_az" {
+module "tlscert-crusc8-uat-platform-pagopa-it-cert_az" {
 
   providers = {
     azurerm = azurerm.uat
   }
 
   source = "./.terraform/modules/__azdo__/azuredevops_build_definition_tls_cert_federated"
-  count = var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
+
+  count = var.tlscert-crusc8-uat-platform-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
-  repository                   = var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.repository
-  path                         = var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.pipeline.path
+  repository                   = var.tlscert-crusc8-uat-platform-pagopa-it.repository
+  path                         = var.tlscert-crusc8-uat-platform-pagopa-it.pipeline.path
   github_service_connection_id = data.azuredevops_serviceendpoint_github.github_ro.id
 
-  dns_record_name                      = var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.pipeline.dns_record_name
-  dns_zone_name                        = var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.pipeline.dns_zone_name
-  dns_zone_resource_group              = var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.pipeline.dns_zone_resource_group
-  tenant_id                            = local.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.tenant_id
-  subscription_name                    = local.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.subscription_name
-  subscription_id                      = local.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.subscription_id
+  dns_record_name                      = var.tlscert-crusc8-uat-platform-pagopa-it.pipeline.dns_record_name
+  dns_zone_name                        = var.tlscert-crusc8-uat-platform-pagopa-it.pipeline.dns_zone_name
+  dns_zone_resource_group              = var.tlscert-crusc8-uat-platform-pagopa-it.pipeline.dns_zone_resource_group
+  tenant_id                            = local.tlscert-crusc8-uat-platform-pagopa-it.tenant_id
+  subscription_name                    = local.tlscert-crusc8-uat-platform-pagopa-it.subscription_name
+  subscription_id                      = local.tlscert-crusc8-uat-platform-pagopa-it.subscription_id
   location                             = local.location_westeurope
   credential_key_vault_name            = local.uat_crusc8_key_vault_name
   credential_key_vault_resource_group  = local.uat_crusc8_key_vault_resource_group
   managed_identity_resource_group_name = local.uat_identity_rg_name
 
   variables = merge(
-    var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.pipeline.variables,
-    local.tlscert-crusc8-itn-internal-uat-platform-pagopa-it-variables,
+    var.tlscert-crusc8-uat-platform-pagopa-it.pipeline.variables,
+    local.tlscert-crusc8-uat-platform-pagopa-it-variables,
   )
 
   variables_secret = merge(
-    var.tlscert-crusc8-itn-internal-uat-platform-pagopa-it.pipeline.variables_secret,
-    local.tlscert-crusc8-itn-internal-uat-platform-pagopa-it-variables_secret,
+    var.tlscert-crusc8-uat-platform-pagopa-it.pipeline.variables_secret,
+    local.tlscert-crusc8-uat-platform-pagopa-it-variables_secret,
   )
 
   service_connection_ids_authorization = [
@@ -77,7 +78,7 @@ module "tlscert-crusc8-itn-internal-uat-platform-pagopa-it-cert_az" {
   ]
 
   schedules = {
-    days_to_build              = ["Wed", "Fri"]
+    days_to_build              = ["Fri"]
     schedule_only_with_changes = false
     start_hours                = 3
     start_minutes              = 0
@@ -87,4 +88,5 @@ module "tlscert-crusc8-itn-internal-uat-platform-pagopa-it-cert_az" {
       exclude = []
     }
   }
+
 }
