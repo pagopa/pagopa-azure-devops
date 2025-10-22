09b5a21ba1f80c3c163086d51baeb9057cb2d1d2
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-10-22 17:58:21 +0200

    fix: tls pipeline for ecommerce

--- azure-devops/ecommerce/05_tlscert-uat-uat-ecommerce-pagopa-it.tf
+++ azure-devops/ecommerce/05_tlscert-uat-uat-ecommerce-pagopa-it.tf
@@ -43,10 +43,10 @@ module "tlscert-uat-ecommerce-pagopa-it-cert_az" {
     azurerm = azurerm.uat
   }
 
-  source = "../core/.terraform/modules/__azdo__/azuredevops_build_definition_tls_cert_federated"
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert_federated?ref=v5.5.1"
   count  = var.tlscert-uat-ecommerce-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
 
-  project_id                   = var.project_name.id
+  project_id                   = data.azuredevops_project.project.id
   repository                   = var.tlscert-uat-ecommerce-pagopa-it.repository
   path                         = var.tlscert-uat-ecommerce-pagopa-it.pipeline.path
   github_service_connection_id = data.azuredevops_serviceendpoint_github.github_rw.id
@@ -62,7 +62,7 @@ module "tlscert-uat-ecommerce-pagopa-it-cert_az" {
 
   location                            = local.location
   credential_key_vault_name           = local.uat_ecommerce_key_vault_name
-  credential_key_vault_resource_group = local.uat_key_vault_resource_group
+  credential_key_vault_resource_group = local.uat_ecommerce_key_vault_resource_group
 
   variables = merge(
     var.tlscert-uat-ecommerce-pagopa-it.pipeline.variables,
@@ -81,8 +81,8 @@ module "tlscert-uat-ecommerce-pagopa-it-cert_az" {
   schedules = {
     days_to_build              = ["Fri"]
     schedule_only_with_changes = false
-    start_hours                = 5
-    start_minutes              = 30
+    start_hours                = 8
+    start_minutes              = 0
     time_zone                  = "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna"
     branch_filter = {
       include = ["master"]
