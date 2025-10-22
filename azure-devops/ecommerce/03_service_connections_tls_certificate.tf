624e522869fbb23ca9c6ccfd2b6d509d2cd5b92e
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-10-22 19:24:08 +0200

    feat: CHK-4547 add ecommerce certificate pipeline (#451)
    * feat: add ecommerce certificate pipeline
    
    * fix: fix key vault name references
    
    * fix: update kv name in tls ecommerce dns
    
    * fix: minor related to tf config
    
    * fix: tls pipeline for dev ecommerce
    
    * fix: tls pipeline for ecommerce
    
    * fix: tls pipeline for prod ecommerce
    
    * chore: update docs
    
    ---------
    
    Co-authored-by: Simone Infante <52280205+infantesimone@users.noreply.github.com>

---
+++ azure-devops/ecommerce/03_service_connections_tls_certificate.tf
@@ -0,0 +1,131 @@
+#
+# DEV
+#
+module "DEV-ECOMMERCE-TLS-CERT-SERVICE-CONN" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.5.1"
+
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-${local.domain}-d-tls-cert-azdo"
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = var.dev_subscription_name
+  subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+
+  location            = local.location
+  resource_group_name = local.dev_identity_rg_name
+}
+
+resource "azurerm_key_vault_access_policy" "DEV-ECOMMERCE-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.dev
+  key_vault_id = data.azurerm_key_vault.domain_kv_dev.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.DEV-ECOMMERCE-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
+
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_dev" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+  prefix            = local.prefix
+  env               = "d"
+  key_vault_name    = local.dev_ecommerce_key_vault_name
+  subscription_name = var.dev_subscription_name
+}
+
+#
+# UAT
+#
+
+module "UAT-ECOMMERCE-TLS-CERT-SERVICE-CONN" {
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.5.1"
+
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-${local.domain}-u-tls-cert-azdo"
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = var.uat_subscription_name
+  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+
+  location            = local.location
+  resource_group_name = local.uat_identity_rg_name
+}
+
+resource "azurerm_key_vault_access_policy" "UAT-ECOMMERCE-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.uat
+  key_vault_id = data.azurerm_key_vault.domain_kv_uat.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.UAT-ECOMMERCE-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
+
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_uat" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
+
+  providers = {
+    azurerm = azurerm.uat
+  }
+  prefix            = local.prefix
+  env               = "u"
+  key_vault_name    = local.uat_ecommerce_key_vault_name
+  subscription_name = var.uat_subscription_name
+}
+
+#
+# PROD
+#
+
+module "PROD-ECOMMERCE-TLS-CERT-SERVICE-CONN" {
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v5.5.1"
+
+  project_id        = data.azuredevops_project.project.id
+  name              = "${local.prefix}-${local.domain}-p-tls-cert-azdo"
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = var.prod_subscription_name
+  subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+
+  location            = local.location
+  resource_group_name = local.prod_identity_rg_name
+}
+
+resource "azurerm_key_vault_access_policy" "PROD-ECOMMERCE-TLS-CERT-SERVICE-CONN_kv_access_policy" {
+  provider     = azurerm.prod
+  key_vault_id = data.azurerm_key_vault.domain_kv_prod.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.PROD-ECOMMERCE-TLS-CERT-SERVICE-CONN.service_principal_object_id
+
+  certificate_permissions = ["Get", "Import"]
+}
+
+# create let's encrypt credential used to create SSL certificates
+module "letsencrypt_prod" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//letsencrypt_credential?ref=v7.30.0"
+
+  providers = {
+    azurerm = azurerm.prod
+  }
+  prefix            = local.prefix
+  env               = "p"
+  key_vault_name    = local.prod_ecommerce_key_vault_name
+  subscription_name = var.prod_subscription_name
+}
---
+++ azure-devops/ecommerce/05_tlscert-dev-dev-ecommerce-pagopa-it.tf
@@ -0,0 +1,92 @@
+variable "tlscert-dev-dev-ecommerce-pagopa-it" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "le-azure-acme-tiny"
+      branch_name    = "refs/heads/master"
+      pipelines_path = "."
+    }
+    pipeline = {
+      enable_tls_cert         = true
+      path                    = "TLS-Certificates\\DEV"
+      dns_record_name         = ""
+      dns_zone_name           = "dev.ecommerce.pagopa.it"
+      dns_zone_resource_group = "pagopa-d-vnet-rg"
+      # common variables to all pipelines
+      variables = {
+        CERT_NAME_EXPIRE_SECONDS = "2592000" #30 days
+        KEY_VAULT_NAME           = "pagopa-d-ecommerce-kv"
+      }
+      # common secret variables to all pipelines
+      variables_secret = {
+      }
+    }
+  }
+}
+
+locals {
+  tlscert-dev-dev-ecommerce-pagopa-it = {
+    tenant_id         = data.azurerm_client_config.current.tenant_id
+    subscription_name = "DEV-PAGOPA"
+    subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+  }
+  tlscert-dev-dev-ecommerce-pagopa-it-variables = {
+    KEY_VAULT_SERVICE_CONNECTION = module.DEV-ECOMMERCE-TLS-CERT-SERVICE-CONN.service_endpoint_name
+  }
+  tlscert-dev-dev-ecommerce-pagopa-it-variables_secret = {
+  }
+}
+
+module "tlscert-dev-dev-ecommerce-pagopa-it-cert_az" {
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert_federated?ref=v5.5.1"
+  count  = var.tlscert-dev-dev-ecommerce-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
+
+  project_id = data.azuredevops_project.project.id
+  repository = var.tlscert-dev-dev-ecommerce-pagopa-it.repository
+  #tfsec:ignore:GEN003
+  path                         = var.tlscert-dev-dev-ecommerce-pagopa-it.pipeline.path
+  github_service_connection_id = data.azuredevops_serviceendpoint_github.github_rw.id
+
+  dns_record_name                      = var.tlscert-dev-dev-ecommerce-pagopa-it.pipeline.dns_record_name
+  dns_zone_name                        = var.tlscert-dev-dev-ecommerce-pagopa-it.pipeline.dns_zone_name
+  dns_zone_resource_group              = var.tlscert-dev-dev-ecommerce-pagopa-it.pipeline.dns_zone_resource_group
+  tenant_id                            = local.tlscert-dev-dev-ecommerce-pagopa-it.tenant_id
+  subscription_name                    = local.tlscert-dev-dev-ecommerce-pagopa-it.subscription_name
+  subscription_id                      = local.tlscert-dev-dev-ecommerce-pagopa-it.subscription_id
+  managed_identity_resource_group_name = local.dev_identity_rg_name
+
+
+  location                            = local.location
+  credential_key_vault_name           = local.dev_ecommerce_key_vault_name
+  credential_key_vault_resource_group = local.dev_ecommerce_key_vault_resource_group
+
+  variables = merge(
+    var.tlscert-dev-dev-ecommerce-pagopa-it.pipeline.variables,
+    local.tlscert-dev-dev-ecommerce-pagopa-it-variables,
+  )
+
+  variables_secret = merge(
+    var.tlscert-dev-dev-ecommerce-pagopa-it.pipeline.variables_secret,
+    local.tlscert-dev-dev-ecommerce-pagopa-it-variables_secret,
+  )
+
+  service_connection_ids_authorization = [
+    module.DEV-ECOMMERCE-TLS-CERT-SERVICE-CONN.service_endpoint_id,
+  ]
+
+  schedules = {
+    days_to_build              = ["Fri"]
+    schedule_only_with_changes = false
+    start_hours                = 8
+    start_minutes              = 0
+    time_zone                  = "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna"
+    branch_filter = {
+      include = ["master"]
+      exclude = []
+    }
+  }
+}
