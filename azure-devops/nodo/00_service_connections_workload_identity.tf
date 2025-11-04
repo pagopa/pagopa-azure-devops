ff992381c5072a920fdeb997509306efb4701822
Matteo Raiola <matteo.raiola@nttdata.com>
2025-11-04 13:06:16 +0100

    feat: [PIDM-1127] add nodo workload identity

--- azure-devops/nodo/00_service_connections_workload_identity.tf
+++ azure-devops/nodo/00_service_connections_workload_identity.tf
@@ -15,3 +15,13 @@ data "azuredevops_serviceendpoint_azurecr" "prod_weu_workload_identity" {
   project_id            = data.azuredevops_project.project.id
   service_endpoint_name = var.acr_weu_service_connection_workload_identity_prod
 }
+
+data "azurerm_user_assigned_identity" "dev_weu_nodo_workload_identity" {
+  name                = "nodo-workload-identity"
+  resource_group_name = "var.aks_dev_platform_name"
+}
+
+data "azurerm_user_assigned_identity" "uat_weu_nodo_workload_identity" {
+  name                = "nodo-workload-identity"
+  resource_group_name = "var.aks_uat_platform_name"
+}
