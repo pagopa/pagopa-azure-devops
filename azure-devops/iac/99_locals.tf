28f95ed9194d5345c0f1a2a149cb72f24e516ff4
Marco Mari <130982006+mamari90@users.noreply.github.com>
2026-03-30 10:50:10 +0200

    fix: Defined liquibase addon pipeline (#463)
    defined liquibase addon pipeline

--- azure-devops/iac/99_locals.tf
+++ azure-devops/iac/99_locals.tf
@@ -97,6 +97,13 @@ locals {
         yml_file_name : "db-metabase-pipelines.yml"
       }
     },
+    "liquibase-addon": {
+      pipeline_prefix : "liquibase-addon",
+      pipeline_path : "database",
+      repository : {
+        yml_file_name : "db-addon-migration-pipelines.yml"
+      }
+    }
 
     // FdR-Fase3 Archive DB pipeline
     "fdr-fase3-archive-db-schema-pipelines" : {
