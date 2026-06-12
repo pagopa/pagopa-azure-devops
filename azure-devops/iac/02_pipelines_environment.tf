068220a2c0a1eccd7143b149c3da6c32ce6dc4a3
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2026-06-12 10:59:17 +0200

    feat: Add client certs iac pipeline (#472)
    * add client certs iac pipeline and devops autoapprove env
    
    * pre-commit

--- azure-devops/iac/02_pipelines_environment.tf
+++ azure-devops/iac/02_pipelines_environment.tf
@@ -4,3 +4,10 @@ resource "azuredevops_environment" "environments" {
   project_id = azuredevops_project.project.id
   name       = each.key
 }
+
+resource "azuredevops_environment" "environments_auto" {
+  for_each = toset(var.pipeline_environments)
+
+  project_id = azuredevops_project.project.id
+  name       = "${each.key}_auto"
+}
