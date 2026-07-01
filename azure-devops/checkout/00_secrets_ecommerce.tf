11c9574b85f238f3594efeb8dedd4860d6a67af2
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2026-07-01 14:15:45 +0200

    feat: [PIDM-2167] add uncommented uat and prod modules

--- azure-devops/checkout/00_secrets_ecommerce.tf
+++ azure-devops/checkout/00_secrets_ecommerce.tf
@@ -17,3 +17,35 @@ module "ecommerce_dev_secrets" {
     "npg-api-key",
   ]
 }
+
+# module "ecommerce_uat_secrets" {
+#
+#   providers = {
+#     azurerm = azurerm.uat
+#   }
+#
+#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.42.3"
+#
+#   resource_group = local.uat_ecommerce_key_vault_resource_group
+#   key_vault_name = local.uat_ecommerce_key_vault_name
+#
+#   secrets = [
+#     "npg-api-key",
+#   ]
+# }
+
+# module "ecommerce_prod_secrets" {
+#
+#   providers = {
+#     azurerm = azurerm.prod
+#   }
+#
+#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.42.3"
+#
+#   resource_group = local.prod_ecommerce_key_vault_resource_group
+#   key_vault_name = local.prod_ecommerce_key_vault_name
+#
+#   secrets = [
+#     "npg-api-key",
+#   ]
+# }
