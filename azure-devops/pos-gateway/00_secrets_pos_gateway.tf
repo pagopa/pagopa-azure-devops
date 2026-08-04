69455b1187fb591971715497cc5597ff80373161
pietro.tota <pietro.tota@pagopa.it>
2026-08-04 15:52:31 +0200

    feat: add tlscert pipelines

---
+++ azure-devops/pos-gateway/00_secrets_pos_gateway.tf
@@ -0,0 +1,17 @@
+module "pos_gateway_dev_secrets" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v7.30.0"
+
+  resource_group = local.dev_pos_gateway_key_vault_resource_group
+  key_vault_name = local.dev_pos_gateway_key_vault_name
+
+  secrets = [
+    "pagopa-d-itn-dev-aks-azure-devops-sa-token",
+    "pagopa-d-itn-dev-aks-azure-devops-sa-cacrt",
+    "pagopa-d-itn-dev-aks-apiserver-url"
+  ]
+}
