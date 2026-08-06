0c65c8f43ae5637e24661ad9270162b5ed1c75b1
pietro.tota <pietro.tota@pagopa.it>
2026-08-06 14:49:14 +0200

    fix: update variables

--- azure-devops/posgw/99_variables.tf
+++ azure-devops/posgw/99_variables.tf
@@ -28,7 +28,7 @@ locals {
   uat_key_vault_resource_group  = "${local.prefix}-u-sec-rg"
   prod_key_vault_resource_group = "${local.prefix}-p-sec-rg"
 
-  #pagopa-d-itn-pos-gateway-sec-rg
+  #pagopa-d-itn-posgw-sec-rg
   dev_posgw_key_vault_resource_group  = "${local.prefix}-d-${local.location_short}-${local.domain}-sec-rg"
   uat_posgw_key_vault_resource_group  = "${local.prefix}-u-${local.location_short}-${local.domain}-sec-rg"
   prod_posgw_key_vault_resource_group = "${local.prefix}-p-${local.location_short}-${local.domain}-sec-rg"
