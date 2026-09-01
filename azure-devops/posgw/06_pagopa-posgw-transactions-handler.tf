153b553b2c1a3355f1325dd3ee8a24e408e8307e
pietro.tota <pietro.tota@pagopa.it>
2026-09-01 12:15:39 +0200

    chore: comment out uat and prod envs

--- azure-devops/posgw/06_pagopa-posgw-transactions-handler.tf
+++ azure-devops/posgw/06_pagopa-posgw-transactions-handler.tf
@@ -47,22 +47,22 @@ locals {
     github_connection = data.azuredevops_serviceendpoint_github.github_rw.service_endpoint_name
 
     # acr section
-    k8s_image_repository_name            = replace(var.pagopa-posgw-transactions-handler.repository.name, "-", "")
-    dev_container_registry_service_conn  = data.azuredevops_serviceendpoint_azurecr.dev_ita_workload_identity.id
-    dev_container_registry_name          = data.azuredevops_serviceendpoint_azurecr.dev_ita_workload_identity.service_endpoint_name
-    uat_container_registry_service_conn  = data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.id
-    uat_container_registry_name          = data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.service_endpoint_name
-    prod_container_registry_service_conn = data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.id
-    prod_container_registry_name         = data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.service_endpoint_name
+    k8s_image_repository_name           = replace(var.pagopa-posgw-transactions-handler.repository.name, "-", "")
+    dev_container_registry_service_conn = data.azuredevops_serviceendpoint_azurecr.dev_ita_workload_identity.id
+    dev_container_registry_name         = data.azuredevops_serviceendpoint_azurecr.dev_ita_workload_identity.service_endpoint_name
+    # uat_container_registry_service_conn  = data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.id
+    # uat_container_registry_name          = data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.service_endpoint_name
+    # prod_container_registry_service_conn = data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.id
+    # prod_container_registry_name         = data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.service_endpoint_name
 
     # aks section
     dev_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_dev.id
     # uat_kubernetes_service_conn  = azuredevops_serviceendpoint_kubernetes.aks_uat.id
     # prod_kubernetes_service_conn = azuredevops_serviceendpoint_kubernetes.aks_prod.id
 
-    dev_container_namespace  = "pagopadcommonacr.azurecr.io"
-    uat_container_namespace  = "pagopaucommonacr.azurecr.io"
-    prod_container_namespace = "pagopapcommonacr.azurecr.io"
+    dev_container_namespace = "pagopadcommonacr.azurecr.io"
+    # uat_container_namespace  = "pagopaucommonacr.azurecr.io"
+    # prod_container_namespace = "pagopapcommonacr.azurecr.io"
 
   }
   # deploy secrets
@@ -121,10 +121,10 @@ module "pagopa-posgw-transactions-handler_deploy" {
   service_connection_ids_authorization = [
     data.azuredevops_serviceendpoint_github.github_ro.id,
     data.azuredevops_serviceendpoint_azurecr.dev_ita_workload_identity.id,
-    data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.id,
-    data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.id,
+    # data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.id,
+    # data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.id,
     data.azuredevops_serviceendpoint_azurerm.dev.id,
-    data.azuredevops_serviceendpoint_azurerm.uat.id,
-    data.azuredevops_serviceendpoint_azurerm.prod.id,
+    # data.azuredevops_serviceendpoint_azurerm.uat.id,
+    # data.azuredevops_serviceendpoint_azurerm.prod.id,
   ]
 }
