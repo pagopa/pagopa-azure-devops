f52f1d2a0299f77c79cdfc9747b343db1dfbd2a4
Samuele Varianti <128470180+svariant@users.noreply.github.com>
2026-09-11 10:50:14 +0200

    fix: [PIDM-2390] [PRINTIT] Fix performance test subkey (#494)
    * fix: [PIDM-2390] [PRINTIT] Fix performance test subkey
    
    * fix: [PIDM-2390] [PRINTIT] Clean up and add secret to keyvault
    
    * fix: [PIDM-2390] [PRINTIT] remove performance test api key from PROD
    
    * fix: [PIDM-2390] [PRINTIT] remove performance test api key from PROD
    
    * precommit
    
    ---------
    
    Co-authored-by: Jacopo Carlini <jacopo.carlini@gmail.com>

--- azure-devops/printit/00_secrets_printit.tf
+++ azure-devops/printit/00_secrets_printit.tf
@@ -16,6 +16,7 @@ module "printit_dev_secrets" {
     "institutions-storage-account-connection-string",
     "notices-storage-account-connection-string",
     "notices-mongo-connection-string",
+    "performance-test-subkey-ec-demo"
   ]
 }
 
@@ -37,6 +38,7 @@ module "printit_uat_secrets" {
     "institutions-storage-account-connection-string",
     "notices-storage-account-connection-string",
     "notices-mongo-connection-string",
+    "performance-test-subkey-ec-demo"
   ]
 }
 
@@ -57,55 +59,6 @@ module "printit_prod_secrets" {
     "pagopa-p-itn-prod-aks-apiserver-url",
     "institutions-storage-account-connection-string",
     "notices-storage-account-connection-string",
-    "notices-mongo-connection-string",
-  ]
-}
-
-
-module "general_dev_secrets" {
-
-  providers = {
-    azurerm = azurerm.dev
-  }
-
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.22.0"
-
-  resource_group = "pagopa-d-sec-rg"
-  key_vault_name = "pagopa-d-kv"
-
-  secrets = [
-    "integration-test-subkey",
-  ]
-}
-
-module "general_uat_secrets" {
-
-  providers = {
-    azurerm = azurerm.uat
-  }
-
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.22.0"
-
-  resource_group = "pagopa-u-sec-rg"
-  key_vault_name = "pagopa-u-kv"
-
-  secrets = [
-    "integration-test-subkey",
-  ]
-}
-
-module "general_prod_secrets" {
-
-  providers = {
-    azurerm = azurerm.prod
-  }
-
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v8.22.0"
-
-  resource_group = "pagopa-p-sec-rg"
-  key_vault_name = "pagopa-p-kv"
-
-  secrets = [
-    "integration-test-subkey",
+    "notices-mongo-connection-string"
   ]
 }
